f6adbf8982e511422f4843a1ab4e375e5ebff2a2
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-26 06:55:26 -0500

    ts key

---
+++ ts/satored-lib/src/key.ts
@@ -0,0 +1,30 @@
+import secp256k1 from 'secp256k1'
+import crypto from 'crypto'
+
+export class Key {
+  private _privateKey: Uint8Array
+  private _publicKey: Uint8Array
+
+  constructor(privateKey: Uint8Array) {
+    this._privateKey = privateKey
+    this._publicKey = secp256k1.publicKeyCreate(privateKey)
+  }
+
+  get privateKey(): Uint8Array {
+    return this._privateKey
+  }
+
+  get publicKey(): Uint8Array {
+    return this._publicKey
+  }
+
+  singleHash(data: Uint8Array): Uint8Array {
+    const res = crypto.createHash('sha256').update(data).digest()
+    const uintArray = new Uint8Array(res.buffer)
+    return uintArray
+  }
+
+  doubleHash(data: Uint8Array): Uint8Array {
+    return this.singleHash(this.singleHash(data))
+  }
+}
