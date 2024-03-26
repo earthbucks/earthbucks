3421ad3d75d29429dfed004cc52ad835de0713eb
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-26 09:52:36 -0500

    rust translation of key compiles

--- ts/satored-lib/src/key.test.ts
+++ ts/satored-lib/src/key.test.ts
@@ -1,10 +1,20 @@
 import { describe, expect, test } from '@jest/globals'
 import Key from './key'
 
-test('Key', () => {
-  const key = Key.fromRandom()
-  expect(key.privateKey).toBeDefined()
-  expect(key.publicKey).toBeDefined()
-  expect(key.singleAddress()).toBeDefined()
-  expect(key.doubleAddress()).toBeDefined()
+
+describe('Key', () => {
+  test('Key', () => {
+    const key = Key.fromRandom()
+    expect(key.privateKey).toBeDefined()
+    expect(key.publicKey).toBeDefined()
+    expect(key.singleAddress()).toBeDefined()
+    expect(key.doubleAddress()).toBeDefined()
+  })
+
+  describe('singleHash', () => {
+    test('defined', () => {
+      const data = new Uint8Array(32)
+      expect(Key.singleHash(data)).toBeDefined()
+    })
+  })
 })
