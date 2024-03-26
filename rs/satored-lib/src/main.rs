b5c7315c436b3125edab17a2d879508e78bef4b5
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-26 09:55:36 -0500

    print privkey/pubkey in rust

--- rs/satored-lib/src/main.rs
+++ rs/satored-lib/src/main.rs
@@ -60,5 +60,10 @@ impl Key {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let key = Key::from_random();
+    let private_key_hex = key.private_key().to_string();
+    let public_key_hex = key.public_key().to_string();
+
+    println!("Private key: {}", private_key_hex);
+    println!("Public key: {}", public_key_hex);
 }
