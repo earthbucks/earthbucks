34b7d6ecdb3d293f12bcb9c2e5028bc299241091
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-27 12:53:14 -0500

    add typescript buffer writer. untested.

---
+++ ts/satored-lib/src/buffer-writer.ts
@@ -0,0 +1,184 @@
+/**
+ * Buffer Writer
+ * =============
+ *
+ * This is the writing complement of the Br. You can easily write
+ * VarInts and other basic number types. The way to use it is: buf =
+ * new Bw().write(buf1).write(buf2).toBuffer()
+ */
+'use strict'
+
+class Bw {
+  bufs: Buffer[]
+
+  constructor(bufs?: Buffer[]) {
+    this.bufs = bufs || []
+  }
+
+  getLength(): number {
+    let len = 0
+    for (const buf of this.bufs) {
+      len += buf.length
+    }
+    return len
+  }
+  toBuffer(): Buffer {
+    return Buffer.concat(this.bufs)
+  }
+
+  write(buf: Buffer): this {
+    this.bufs.push(buf)
+    return this
+  }
+
+  writeReverse(buf: Buffer): this {
+    const buf2 = Buffer.alloc(buf.length)
+    for (let i = 0; i < buf2.length; i++) {
+      buf2[i] = buf[buf.length - 1 - i]
+    }
+    this.bufs.push(buf2)
+    return this
+  }
+
+  writeUInt8(n: number): this {
+    const buf = Buffer.alloc(1)
+    buf.writeUInt8(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeInt8(n: number): this {
+    const buf = Buffer.alloc(1)
+    buf.writeInt8(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt16BE(n: number): this {
+    const buf = Buffer.alloc(2)
+    buf.writeUInt16BE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeInt16BE(n: number): this {
+    const buf = Buffer.alloc(2)
+    buf.writeInt16BE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt16LE(n: number): this {
+    const buf = Buffer.alloc(2)
+    buf.writeUInt16LE(n, 0)
+    this.write(buf)
+    return this
+  }
+  writeInt16LE(n: number): this {
+    const buf = Buffer.alloc(2)
+    buf.writeInt16LE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt32BE(n: number): this {
+    const buf = Buffer.alloc(4)
+    buf.writeUInt32BE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeInt32BE(n: number): this {
+    const buf = Buffer.alloc(4)
+    buf.writeInt32BE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt32LE(n: number): this {
+    const buf = Buffer.alloc(4)
+    buf.writeUInt32LE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeInt32LE(n: number): this {
+    const buf = Buffer.alloc(4)
+    buf.writeInt32LE(n, 0)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt64BEBn(bn: bigint): this {
+    const buf = Buffer.alloc(8)
+    buf.writeBigInt64BE(bn)
+    this.write(buf)
+    return this
+  }
+
+  writeUInt64LEBn(bn: bigint): this {
+    const buf = Buffer.alloc(8)
+    buf.writeBigInt64LE(bn)
+    this.write(buf)
+    return this
+  }
+
+  writeVarIntNum(n: number): this {
+    const buf = Bw.varIntBufNum(n)
+    this.write(buf)
+    return this
+  }
+
+  writeVarIntBn(bn: bigint): this {
+    const buf = Bw.varIntBufBn(bn)
+    this.write(buf)
+    return this
+  }
+
+  static varIntBufNum(n: number): Buffer {
+    let buf: Buffer
+    if (n < 253) {
+      buf = Buffer.alloc(1)
+      buf.writeUInt8(n, 0)
+    } else if (n < 0x10000) {
+      buf = Buffer.alloc(1 + 2)
+      buf.writeUInt8(253, 0)
+      buf.writeUInt16LE(n, 1)
+    } else if (n < 0x100000000) {
+      buf = Buffer.alloc(1 + 4)
+      buf.writeUInt8(254, 0)
+      buf.writeUInt32LE(n, 1)
+    } else {
+      buf = Buffer.alloc(1 + 8)
+      buf.writeUInt8(255, 0)
+      buf.writeInt32LE(n & -1, 1)
+      buf.writeUInt32LE(Math.floor(n / 0x100000000), 5)
+    }
+    return buf
+  }
+
+  static varIntBufBn(bn: bigint): Buffer {
+    let buf: Buffer
+    const n = Number(bn)
+    if (n < 253) {
+      buf = Buffer.alloc(1)
+      buf.writeUInt8(n, 0)
+    } else if (n < 0x10000) {
+      buf = Buffer.alloc(1 + 2)
+      buf.writeUInt8(253, 0)
+      buf.writeUInt16LE(n, 1)
+    } else if (n < 0x100000000) {
+      buf = Buffer.alloc(1 + 4)
+      buf.writeUInt8(254, 0)
+      buf.writeUInt32LE(n, 1)
+    } else {
+      const bw = new Bw()
+      bw.writeUInt8(255)
+      bw.writeUInt64LEBn(bn)
+      buf = bw.toBuffer()
+    }
+    return buf
+  }
+}
+
+export { Bw }
