d935891f1b969fddd1a5746ff41cd344a04cbd11
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-29 04:07:33 -0500

    rust: begin script chunk tests

--- rs/satored_lib/src/script_chunk.rs
+++ rs/satored_lib/src/script_chunk.rs
@@ -17,7 +17,10 @@ impl ScriptChunk {
 
     pub fn to_string(&self) -> Result<String, Box<dyn Error>> {
         match &self.buffer {
-            Some(buffer) => Ok(format!("0x{:02x?}", buffer)),
+            Some(buffer) => {
+                let hex: Vec<String> = buffer.iter().map(|b| format!("{:02x}", b)).collect();
+                Ok(format!("0x{}", hex.join("")))
+            }
             None => {
                 let name = OPCODE_TO_NAME.get(&self.opcode);
                 match name {
@@ -88,29 +91,77 @@ impl ScriptChunk {
     }
 
     pub fn from_u8_vec(&mut self, arr: Vec<u8>) -> Result<(), Box<dyn Error>> {
-      let opcode = arr[0];
-      if opcode == *NAME_TO_OPCODE.get("PUSHDATA1").unwrap() {
-          let len = arr[1] as usize;
-          self.opcode = opcode;
-          self.buffer = Some(arr[2..2+len].to_vec());
-      } else if opcode == *NAME_TO_OPCODE.get("PUSHDATA2").unwrap() {
-          let len = u16::from_be_bytes([arr[1], arr[2]]) as usize;
-          self.opcode = opcode;
-          self.buffer = Some(arr[3..3+len].to_vec());
-      } else if opcode == *NAME_TO_OPCODE.get("PUSHDATA4").unwrap() {
-          let len = u32::from_be_bytes([arr[1], arr[2], arr[3], arr[4]]) as usize;
-          self.opcode = opcode;
-          self.buffer = Some(arr[5..5+len].to_vec());
-      } else {
-          self.opcode = opcode;
-          self.buffer = None;
-      }
-      Ok(())
-  }
+        let opcode = arr[0];
+        if opcode == *NAME_TO_OPCODE.get("PUSHDATA1").unwrap() {
+            let len = arr[1] as usize;
+            self.opcode = opcode;
+            self.buffer = Some(arr[2..2 + len].to_vec());
+        } else if opcode == *NAME_TO_OPCODE.get("PUSHDATA2").unwrap() {
+            let len = u16::from_be_bytes([arr[1], arr[2]]) as usize;
+            self.opcode = opcode;
+            self.buffer = Some(arr[3..3 + len].to_vec());
+        } else if opcode == *NAME_TO_OPCODE.get("PUSHDATA4").unwrap() {
+            let len = u32::from_be_bytes([arr[1], arr[2], arr[3], arr[4]]) as usize;
+            self.opcode = opcode;
+            self.buffer = Some(arr[5..5 + len].to_vec());
+        } else {
+            self.opcode = opcode;
+            self.buffer = None;
+        }
+        Ok(())
+    }
 
-  pub fn from_u8_vec_new(arr: Vec<u8>) -> Result<ScriptChunk, Box<dyn Error>> {
-    let mut chunk = ScriptChunk::new(0, None);
-    chunk.from_u8_vec(arr)?;
-    Ok(chunk)
+    pub fn from_u8_vec_new(arr: Vec<u8>) -> Result<ScriptChunk, Box<dyn Error>> {
+        let mut chunk = ScriptChunk::new(0, None);
+        chunk.from_u8_vec(arr)?;
+        Ok(chunk)
+    }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let opcode = 1;
+        let buffer = Some(vec![2, 3, 4]);
+        let chunk = ScriptChunk::new(opcode, buffer.clone());
+
+        assert_eq!(chunk.opcode, opcode);
+        assert_eq!(chunk.buffer, buffer);
+    }
+
+    #[test]
+    fn test_to_string_if() {
+        let chunk = ScriptChunk::new(*NAME_TO_OPCODE.get("IF").unwrap(), None);
+        assert_eq!(chunk.to_string().unwrap(), "IF");
+    }
+
+    #[test]
+    fn test_to_string_pushdata1() {
+        let chunk = ScriptChunk::new(
+            *NAME_TO_OPCODE.get("PUSHDATA1").unwrap(),
+            Some(vec![1, 2, 3]),
+        );
+        assert_eq!(chunk.to_string().unwrap(), "0x010203");
+    }
+
+    #[test]
+    fn test_to_string_pushdata2() {
+        let chunk = ScriptChunk::new(
+            *NAME_TO_OPCODE.get("PUSHDATA2").unwrap(),
+            Some(vec![4, 5, 6]),
+        );
+        assert_eq!(chunk.to_string().unwrap(), "0x040506");
+    }
+
+    #[test]
+    fn test_to_string_pushdata4() {
+        let chunk = ScriptChunk::new(
+            *NAME_TO_OPCODE.get("PUSHDATA4").unwrap(),
+            Some(vec![7, 8, 9]),
+        );
+        assert_eq!(chunk.to_string().unwrap(), "0x070809");
+    }
 }
