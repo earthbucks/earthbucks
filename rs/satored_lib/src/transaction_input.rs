6db8e671066a2b2169699ee9490556f60be29f85
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-30 09:08:57 -0500

    rust: tx input buffer reader

--- rs/satored_lib/src/transaction_input.rs
+++ rs/satored_lib/src/transaction_input.rs
@@ -23,21 +23,32 @@ impl TransactionInput {
     pub fn from_u8_vec(buf: Vec<u8>) -> Result<Self, Box<dyn std::error::Error>> {
         let mut reader = BufferReader::new(buf);
         let input_tx_hash = reader.read(32);
-        let input_tx_index = reader.read_u32_le();
+        let input_tx_index = reader.read_u32_be();
         let size = reader.read_u8() as usize;
         let script = Script::from_u8_vec_new(reader.read(size as usize).as_slice())?;
-        let sequence = reader.read_u32_le();
+        let sequence = reader.read_u32_be();
+        Ok(Self::new(input_tx_hash, input_tx_index, script, sequence))
+    }
+
+    pub fn from_buffer_reader(
+        reader: &mut BufferReader,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let input_tx_hash = reader.read(32);
+        let input_tx_index = reader.read_u32_be();
+        let size = reader.read_var_int() as usize;
+        let script = Script::from_u8_vec_new(reader.read(size as usize).as_slice())?;
+        let sequence = reader.read_u32_be();
         Ok(Self::new(input_tx_hash, input_tx_index, script, sequence))
     }
 
     pub fn to_u8_vec(&self) -> Vec<u8> {
         let mut writer = BufferWriter::new();
         writer.write_u8_vec(self.input_tx_hash.clone());
-        writer.write_u32_le(self.input_tx_index);
+        writer.write_u32_be(self.input_tx_index);
         let script_buf = self.script.to_u8_vec();
         writer.write_u8_vec(VarInt::from_u64_new(script_buf.len() as u64).to_u8_vec());
         writer.write_u8_vec(script_buf);
-        writer.write_u32_le(self.sequence);
+        writer.write_u32_be(self.sequence);
         writer.to_u8_vec()
     }
 }
@@ -81,5 +92,40 @@ mod tests {
         }
         assert_eq!(tx_input2.sequence, sequence);
         Ok(())
+    }
+
+    #[test]
+    fn test_from_buffer_reader() {
+        let input_tx_hash = vec![0u8; 32];
+        let input_tx_index = 1u32;
+        let script_hex = "";
+        let script = Script::from_string_new(script_hex);
+        let sequence = 2u32;
+
+        let script_v8_vec = match script {
+            Ok(script) => script.to_u8_vec(),
+            Err(_) => panic!("Failed to convert script to u8 vec"),
+        };
+
+        let mut writer = BufferWriter::new();
+        writer.write_u8_vec(input_tx_hash.clone());
+        writer.write_u32_be(input_tx_index);
+        writer.write_var_int(script_v8_vec.len() as u64);
+        writer.write_u8_vec(script_v8_vec);
+        writer.write_u32_be(sequence);
+
+        let mut reader = BufferReader::new(writer.to_u8_vec());
+        let transaction_input = TransactionInput::from_buffer_reader(&mut reader).unwrap();
+
+        let script2 = transaction_input.script;
+        let script2_hex = match script2.to_string() {
+            Ok(script2_hex) => script2_hex,
+            Err(_) => panic!("Failed to convert script to string"),
+        };
+
+        assert_eq!(transaction_input.input_tx_hash, input_tx_hash);
+        assert_eq!(transaction_input.input_tx_index, input_tx_index);
+        assert_eq!(script2_hex, script_hex);
+        assert_eq!(transaction_input.sequence, sequence);
     }
 }
