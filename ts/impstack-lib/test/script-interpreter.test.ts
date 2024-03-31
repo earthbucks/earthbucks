a272d4789e74f7c389426241bdef046be3cf0ebe
Ryan X. Charles <ryan@ryanxcharles.com>
2024-03-31 05:42:43 -0500

    create standard test vectors for script interpreter

--- ts/impstack-lib/test/script-interpreter.test.ts
+++ ts/impstack-lib/test/script-interpreter.test.ts
@@ -4,6 +4,8 @@ import Script from '../src/script'
 import Transaction from '../src/transaction'
 import TransactionInput from '../src/transaction-input'
 import TransactionOutput from '../src/transaction-output'
+import fs from 'fs'
+import path from 'path'
 
 describe('ScriptInterpreter', () => {
   let transaction: Transaction
@@ -105,6 +107,52 @@ describe('ScriptInterpreter', () => {
         scriptInterpreter.returnValue &&
           Buffer.from(scriptInterpreter.returnValue).toString('hex'),
       ).toEqual('ff')
+    })
+  })
+
+  describe('test vectors', () => {
+    interface TestScript {
+      name: string
+      script: string
+      expected_return_value: string
+      expected_success: boolean
+    }
+
+    describe('script interpreter scripts', () => {
+      const filePath = path.resolve(
+        __dirname,
+        '../../../json/script_interpreter.json',
+      )
+      const jsonString = fs.readFileSync(filePath, 'utf-8')
+      const testScripts: TestScript[] = JSON.parse(jsonString).scripts
+      let transaction: Transaction
+
+      beforeEach(() => {
+        transaction = new Transaction(
+          1,
+          [new TransactionInput(new Uint8Array(), 0, new Script(), 0xffffffff)],
+          [new TransactionOutput(BigInt(0), new Script())],
+          BigInt(0),
+        )
+      })
+
+      testScripts.forEach((testScript) => {
+        test(testScript.name, () => {
+          const script = Script.fromString(testScript.script)
+          const scriptInterpreter = ScriptInterpreter.fromScript(
+            script,
+            transaction,
+          )
+          scriptInterpreter.evalScript()
+          expect(scriptInterpreter.returnSuccess).toBe(
+            testScript.expected_success,
+          )
+          expect(
+            scriptInterpreter.returnValue &&
+              Buffer.from(scriptInterpreter.returnValue).toString('hex'),
+          ).toBe(testScript.expected_return_value)
+        })
+      })
     })
   })
 })
