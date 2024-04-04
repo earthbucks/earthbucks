a59d980115a5b464938029950f4093bf6b74eb0b
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-04 03:23:33 -0500

    test sign two inputs in rust

--- rs/impstack_lib/src/transaction_signer.rs
+++ rs/impstack_lib/src/transaction_signer.rs
@@ -150,4 +150,86 @@ mod tests {
         let result = script_interpreter.eval_script();
         assert_eq!(result, true);
     }
+
+    #[test]
+    fn should_sign_two_inputs() {
+        let mut tx_out_map = TransactionOutputMap::new();
+        let mut pub_key_hash_key_map = PubKeyHashKeyMap::new();
+
+        // generate 5 keys, 5 outputs, and add them to the txOutMap
+        for i in 0..5 {
+            let key = Key::from_random();
+            let pub_key_hash = PubKeyHash::new(key.public_key.clone());
+            pub_key_hash_key_map.add(key.clone(), &pub_key_hash.pub_key_hash.clone());
+            let script = Script::from_pub_key_hash_output(&pub_key_hash.pub_key_hash.clone());
+            let output = TransactionOutput::new(100, script);
+            tx_out_map.add(output, vec![0; 32].as_slice(), i);
+        }
+
+        let mut transaction_builder =
+            TransactionBuilder::new(&tx_out_map, Script::from_string("").unwrap());
+        transaction_builder.add_output(100, Script::from_string("").unwrap());
+        transaction_builder.add_output(100, Script::from_string("").unwrap());
+
+        let transaction = transaction_builder.build();
+
+        assert_eq!(transaction.inputs.len(), 2);
+        assert_eq!(transaction.outputs.len(), 2);
+        assert_eq!(transaction.outputs[0].value, 100);
+        assert_eq!(transaction.outputs[1].value, 100);
+
+        let mut transaction_signer =
+            TransactionSigner::new(transaction.clone(), &tx_out_map, &pub_key_hash_key_map);
+        let signed1 = transaction_signer.sign(0);
+        let signed2 = transaction_signer.sign(1);
+        let signed_transaction = transaction_signer.transaction;
+        assert_eq!(signed1, true);
+        assert_eq!(signed2, true);
+
+        let tx_input_1 = &signed_transaction.inputs[0];
+        let tx_output_1 = tx_out_map
+            .get(&tx_input_1.input_tx_id.clone(), tx_input_1.input_tx_index)
+            .unwrap();
+        let exec_script_1 = tx_output_1.script.clone();
+        let sig_buf_1 = tx_input_1.script.chunks[0].buffer.clone().unwrap();
+        assert_eq!(sig_buf_1.len(), 65);
+        let pub_key_buf_1 = tx_input_1.script.chunks[1].buffer.clone().unwrap();
+        assert_eq!(pub_key_buf_1.len(), 33);
+
+        let stack_1 = vec![sig_buf_1, pub_key_buf_1];
+
+        let mut script_interpreter_1 = ScriptInterpreter::from_output_script_transaction(
+            exec_script_1,
+            signed_transaction.clone(),
+            0,
+            stack_1,
+            100,
+        );
+
+        let result_1 = script_interpreter_1.eval_script();
+        assert_eq!(result_1, true);
+
+        let tx_input_2 = &signed_transaction.inputs[1];
+        let tx_output_2 = tx_out_map
+            .get(&tx_input_2.input_tx_id.clone(), tx_input_2.input_tx_index)
+            .unwrap();
+        let exec_script_2 = tx_output_2.script.clone();
+        let sig_buf_2 = tx_input_2.script.chunks[0].buffer.clone().unwrap();
+        assert_eq!(sig_buf_2.len(), 65);
+        let pub_key_buf_2 = tx_input_2.script.chunks[1].buffer.clone().unwrap();
+        assert_eq!(pub_key_buf_2.len(), 33);
+
+        let stack_2 = vec![sig_buf_2, pub_key_buf_2];
+
+        let mut script_interpreter_2 = ScriptInterpreter::from_output_script_transaction(
+            exec_script_2,
+            signed_transaction.clone(),
+            1,
+            stack_2,
+            100,
+        );
+
+        let result_2 = script_interpreter_2.eval_script();
+        assert_eq!(result_2, true);
+    }
 }
