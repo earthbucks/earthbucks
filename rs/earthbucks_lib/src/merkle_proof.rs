c1ce16dc7083cef4760e22d7b273773bf6cc2211
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-06 09:01:40 -0500

    rs: merkle proof tests.

--- rs/earthbucks_lib/src/merkle_proof.rs
+++ rs/earthbucks_lib/src/merkle_proof.rs
@@ -93,3 +93,79 @@ impl MerkleProof {
         (root, proofs)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_proofs_and_root_with_1_data() {
+        let data1 = double_blake3_hash("data1".as_bytes()).to_vec();
+
+        let data = vec![data1.clone()];
+        let (root, proofs) = MerkleProof::generate_proofs_and_root(data);
+        let hex = hex::encode(root.clone());
+        assert_eq!(
+            hex,
+            "689ce4d2c5a083571f0a1b1d8d4bb9a5b5494aba2c98eb606c1d265681ac5244"
+        );
+
+        let proof1 = &proofs[0];
+        let verified1 = MerkleProof::verify_proof(&data1, proof1, &root);
+        assert!(verified1);
+    }
+
+    #[test]
+    fn generate_proofs_and_root_with_2_datas() {
+        let data1 = double_blake3_hash("data1".as_bytes()).to_vec();
+        let data2 = double_blake3_hash("data2".as_bytes()).to_vec();
+
+        let data = vec![data1.clone(), data2.clone()];
+        let (root, proofs) = MerkleProof::generate_proofs_and_root(data);
+        let hex = hex::encode(root.clone());
+        assert_eq!(
+            hex,
+            "fdc77b5c255818023a45501e5a5ce7f2e0ea275546cad26df121d4b8f17d8cde"
+        );
+
+        let proof1 = &proofs[0];
+        let verified1 = MerkleProof::verify_proof(&data1, proof1, &root);
+        assert!(verified1);
+
+        let proof2 = &proofs[1];
+        let verified2 = MerkleProof::verify_proof(&data2, proof2, &root);
+        assert!(verified2);
+    }
+
+    #[test]
+    fn generate_proofs_and_root_with_4_datas() {
+        let data1 = double_blake3_hash("data1".as_bytes()).to_vec();
+        let data2 = double_blake3_hash("data2".as_bytes()).to_vec();
+        let data3 = double_blake3_hash("data3".as_bytes()).to_vec();
+        let data4 = double_blake3_hash("data4".as_bytes()).to_vec();
+
+        let data = vec![data1.clone(), data2.clone(), data3.clone(), data4.clone()];
+        let (root, proofs) = MerkleProof::generate_proofs_and_root(data);
+        let hex = hex::encode(root.clone());
+        assert_eq!(
+            hex,
+            "a3344f480b6c8102dd11ad1b686aa2b890b8455bd5343f66b33d392b05b4f187"
+        );
+
+        let proof1 = &proofs[0];
+        let verified1 = MerkleProof::verify_proof(&data1, proof1, &root);
+        assert!(verified1);
+
+        let proof2 = &proofs[1];
+        let verified2 = MerkleProof::verify_proof(&data2, proof2, &root);
+        assert!(verified2);
+
+        let proof3 = &proofs[2];
+        let verified3 = MerkleProof::verify_proof(&data3, proof3, &root);
+        assert!(verified3);
+
+        let proof4 = &proofs[3];
+        let verified4 = MerkleProof::verify_proof(&data4, proof4, &root);
+        assert!(verified4);
+    }
+}
