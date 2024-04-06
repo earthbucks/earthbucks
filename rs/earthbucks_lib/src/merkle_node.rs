e8537cc660b4499f640f3437892f5ae2f94f12e5
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-06 08:20:01 -0500

    rs: more tests

--- rs/earthbucks_lib/src/merkle_node.rs
+++ rs/earthbucks_lib/src/merkle_node.rs
@@ -65,7 +65,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_u8_vecs() {
+    fn from_u8_vecs_1_data() {
+        let data = double_blake3_hash("data1".as_bytes()).to_vec();
+        let root = MerkleNode::from_u8_vecs(vec![data]);
+        let hex = hex::encode(root.hash());
+        assert_eq!(
+            hex,
+            "689ce4d2c5a083571f0a1b1d8d4bb9a5b5494aba2c98eb606c1d265681ac5244"
+        );
+    }
+
+    #[test]
+    fn from_u8_vecs_2_datas() {
+        let data1 = double_blake3_hash("data1".as_bytes()).to_vec();
+        let data2 = double_blake3_hash("data2".as_bytes()).to_vec();
+
+        let data = vec![data1, data2];
+        let root = MerkleNode::from_u8_vecs(data);
+        let hex = hex::encode(root.hash());
+        assert_eq!(
+            hex,
+            "fdc77b5c255818023a45501e5a5ce7f2e0ea275546cad26df121d4b8f17d8cde"
+        );
+    }
+
+    #[test]
+    fn from_u8_vecs_4_datas() {
         let data1 = double_blake3_hash("data1".as_bytes()).to_vec();
         let data2 = double_blake3_hash("data2".as_bytes()).to_vec();
         let data3 = double_blake3_hash("data3".as_bytes()).to_vec();
