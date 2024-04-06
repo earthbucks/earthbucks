c64b18353101f43f08d5421142f2b55e00555a7a
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-06 05:41:28 -0500

    ts: merkle nodes and merkle proofs

--- ts/earthbucks-lib/src/merkle-node.ts
+++ ts/earthbucks-lib/src/merkle-node.ts
@@ -1,18 +1,85 @@
 import { doubleBlake3Hash } from './blake3'
 
+export class MerkleProof {
+  public root: Uint8Array
+  public proof: Array<[Uint8Array, boolean]>
+
+  constructor(root: Uint8Array, proof: Array<[Uint8Array, boolean]>) {
+    this.root = root
+    this.proof = proof
+  }
+
+  public verify(hashedData: Uint8Array): boolean {
+    let hash = hashedData
+    for (const [sibling, isLeft] of this.proof) {
+      hash = isLeft
+        ? doubleBlake3Hash(Buffer.concat([sibling, hash]))
+        : doubleBlake3Hash(Buffer.concat([hash, sibling]))
+    }
+    return Buffer.compare(hash, this.root) === 0
+  }
+
+  static verifyProof(data: Uint8Array, proof: MerkleProof, root: Uint8Array) {
+    return Buffer.compare(proof.root, root) === 0 || proof.verify(data)
+  }
+
+  static generateProofsAndRoot(
+    hashedDatas: Uint8Array[],
+  ): [Uint8Array, MerkleProof[]] {
+    if (hashedDatas.length === 0) {
+      throw new Error('Cannot create Merkle tree from empty array')
+    }
+    if (hashedDatas.length === 1) {
+      const root = hashedDatas[0]
+      const proof = new MerkleProof(root, [])
+      return [root, [proof]]
+    }
+    if (hashedDatas.length === 2) {
+      const root = doubleBlake3Hash(
+        Buffer.concat([hashedDatas[0], hashedDatas[1]]),
+      )
+      const proofs = [
+        new MerkleProof(root, [[hashedDatas[1], true]]),
+        new MerkleProof(root, [[hashedDatas[0], false]]),
+      ]
+      return [root, proofs]
+    }
+    // Make sure the number of elements is a power of two
+    while ((hashedDatas.length & (hashedDatas.length - 1)) !== 0) {
+      hashedDatas.push(hashedDatas[hashedDatas.length - 1])
+    }
+    const [leftRoot, leftProofs] = MerkleProof.generateProofsAndRoot(
+      hashedDatas.slice(0, hashedDatas.length / 2),
+    )
+    const [rightRoot, rightProofs] = MerkleProof.generateProofsAndRoot(
+      hashedDatas.slice(hashedDatas.length / 2),
+    )
+    const root = doubleBlake3Hash(Buffer.concat([leftRoot, rightRoot]))
+    const proofs = [
+      ...leftProofs.map(
+        (proof) => new MerkleProof(root, [[rightRoot, true], ...proof.proof]),
+      ),
+      ...rightProofs.map(
+        (proof) => new MerkleProof(root, [[leftRoot, false], ...proof.proof]),
+      ),
+    ]
+    return [root, proofs]
+  }
+}
+
 export default class MerkleNode {
   public left: MerkleNode | null
   public right: MerkleNode | null
-  public data: Uint8Array
+  public hashedData: Uint8Array
 
   constructor(
     left: MerkleNode | null,
     right: MerkleNode | null,
-    data: Uint8Array,
+    hashedData: Uint8Array,
   ) {
     this.left = left
     this.right = right
-    this.data = data
+    this.hashedData = hashedData
   }
 
   public hash(): Uint8Array {
@@ -21,20 +88,20 @@ export default class MerkleNode {
       const rightData = this.right ? this.right.hash() : leftData
       return doubleBlake3Hash(Buffer.concat([leftData, rightData]))
     } else {
-      return this.data
+      return this.hashedData
     }
   }
 
-  static fromU8Vecs(datas: Uint8Array[]): MerkleNode {
-    if (datas.length === 0) {
+  static fromU8Vecs(hashedDatas: Uint8Array[]): MerkleNode {
+    if (hashedDatas.length === 0) {
       throw new Error('Cannot create MerkleNode from empty array')
     }
-    if (datas.length === 1) {
-      return new MerkleNode(null, null, datas[0])
+    if (hashedDatas.length === 1) {
+      return new MerkleNode(null, null, hashedDatas[0])
     }
-    if (datas.length === 2) {
-      const left = new MerkleNode(null, null, datas[0])
-      const right = new MerkleNode(null, null, datas[1])
+    if (hashedDatas.length === 2) {
+      const left = new MerkleNode(null, null, hashedDatas[0])
+      const right = new MerkleNode(null, null, hashedDatas[1])
       return new MerkleNode(
         left,
         right,
@@ -42,90 +109,20 @@ export default class MerkleNode {
       )
     }
     // Make sure the number of elements is a power of two
-    while ((datas.length & (datas.length - 1)) !== 0) {
-      datas.push(datas[datas.length - 1])
+    while ((hashedDatas.length & (hashedDatas.length - 1)) !== 0) {
+      hashedDatas.push(hashedDatas[hashedDatas.length - 1])
     }
-    const left = MerkleNode.fromU8Vecs(datas.slice(0, datas.length / 2))
-    const right = MerkleNode.fromU8Vecs(datas.slice(datas.length / 2))
+    const left = MerkleNode.fromU8Vecs(
+      hashedDatas.slice(0, hashedDatas.length / 2),
+    )
+    const right = MerkleNode.fromU8Vecs(
+      hashedDatas.slice(hashedDatas.length / 2),
+    )
 
     return new MerkleNode(
       left,
       right,
       doubleBlake3Hash(Buffer.concat([left.hash(), right.hash()])),
     )
-  }
-
-  // generate proofs for data in the map
-  static fromU8VecsWithProofs(
-    datas: Uint8Array[],
-    proofs: Map<Uint8Array, Uint8Array[]> = new Map(),
-  ): MerkleNode {
-    if (datas.length === 0) {
-      throw new Error('Cannot create MerkleNode from empty array')
-    }
-    if (datas.length === 1) {
-      return new MerkleNode(null, null, datas[0])
-    }
-    if (datas.length === 2) {
-      const left = new MerkleNode(null, null, datas[0])
-      const right = new MerkleNode(null, null, datas[1])
-      return new MerkleNode(
-        left,
-        right,
-        doubleBlake3Hash(Buffer.concat([left.hash(), right.hash()])),
-      )
-    }
-    // Make sure the number of elements is a power of two
-    while ((datas.length & (datas.length - 1)) !== 0) {
-      datas.push(datas[datas.length - 1])
-    }
-    const left = MerkleNode.fromU8VecsWithProofs(datas.slice(0, datas.length / 2))
-    const right = MerkleNode.fromU8VecsWithProofs(datas.slice(datas.length / 2))
-
-    // When creating a new node, add the sibling's data to the proof for the left and right data
-    if (proofs.has(left.data)) {
-      proofs.get(left.data)?.push(right.data)
-    }
-    if (proofs.has(right.data)) {
-      proofs.get(right.data)?.push(left.data)
-    }
-
-    return new MerkleNode(
-      left,
-      right,
-      doubleBlake3Hash(Buffer.concat([left.hash(), right.hash()])),
-    )
-  }
-
-  // This method generates an inclusion proof for the given data.
-  public generateProof(data: Uint8Array): Uint8Array[] | null {
-    // if (this.data.equals(data)) {
-    if (Buffer.compare(this.data, data) === 0) {
-      return []
-    } else if (this.left && this.right) {
-      const leftProof = this.left.generateProof(data)
-      if (leftProof) {
-        return [this.right.data, ...leftProof]
-      }
-      const rightProof = this.right.generateProof(data)
-      if (rightProof) {
-        return [this.left.data, ...rightProof]
-      }
-    }
-    return null
-  }
-
-  // This method verifies an inclusion proof for the given data and root.
-  public static verifyProof(
-    data: Uint8Array,
-    proof: Uint8Array[],
-    root: Uint8Array,
-  ): boolean {
-    let hash = data
-    for (const sibling of proof) {
-      hash = doubleBlake3Hash(Buffer.concat([hash, sibling]))
-    }
-    // return hash.equals(root)
-    return Buffer.compare(hash, root) === 0
   }
 }
