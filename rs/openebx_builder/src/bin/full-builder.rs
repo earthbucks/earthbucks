a0f82e09a87e329eee577fe6df24040b52992fea
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-11 04:15:36 -0500

    load and print env vars

--- rs/openebx_builder/src/bin/full-builder.rs
+++ rs/openebx_builder/src/bin/full-builder.rs
@@ -1,6 +1,14 @@
+use dotenv::dotenv;
+use std::env;
 use openebx_lib::key::Key;
 
 fn main() {
+    dotenv().ok();
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+
     let key = Key::from_random();
     let priv_key = key.private_key;
     let priv_key_hex = hex::encode(priv_key);
