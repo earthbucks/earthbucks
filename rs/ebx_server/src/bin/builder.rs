075a12089bf3047c14c412056380d830ee3cba75
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-11 07:42:55 -0500

    work on pseudocode for builder

--- rs/ebx_server/src/bin/builder.rs
+++ rs/ebx_server/src/bin/builder.rs
@@ -15,11 +15,33 @@ fn main() {
     println!("Private key: {}", priv_key_hex);
 
     // load config
-    // get the longest (validated) chain
-    // get all transactions not in the longest chain (unconfirmed transactions)
-    // compute merkle root of unconfirmed transactions
-    // build block header with correct merkle root - but (probably) insufficient difficulty
-    // await new PoW from users
-    // build block with PoW
-    // broadcast block
+    // memory: longest chain (headers of validated blocks)
+    // memory: longest chain tip ("chain tip")
+    // memory: unconfirmed transactions ("mempool")
+    // poll db every 1 second
+    //   - get the longest (validated) chain
+    //   - if chain tip has changed:
+    //     - validate new chain
+    //     - if new chain is valid:
+    //       - broadcast new chain
+    //       - if reorg:
+    //         - perform reorg
+    //       - continue
+    //     - else:
+    //       - penalize nodes that broadcasted invalid chain
+    //       - continue
+    //   - else (chain tip hasn't changed):
+    //     - if valid PoW has been found:
+    //       - add block to chain and broadcast
+    //       - continue
+    //     - else if unconfirmed transaction set is changed:
+    //       - get all transactions not in the longest chain (unconfirmed transactions)
+    //       - compute merkle root of unconfirmed transactions
+    //       - build block header with correct merkle root - but (probably) insufficient difficulty
+    //       - await new PoW from users
+    //       - build block with PoW
+    //       - broadcast block
+    //       - continue
+    //     - else:
+    //       - continue
 }
