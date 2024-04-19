0b5c2911d9fd842578ff78c14be2ba15fb53351c
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-19 04:09:23 -0500

    EBX Pool logo, favicon, text, landing page

--- ts/ebx-pool/app/routes/_index.tsx
+++ ts/ebx-pool/app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'OpenEBX' },
-    { name: 'description', content: 'Welcome to OpenEBX!' },
+    { title: 'EBX Pool' },
+    { name: 'description', content: 'Welcome to EBX Pool!' },
   ]
 }
 
@@ -14,46 +14,36 @@ export default function Index() {
         <div className="mx-auto">
           <div className="inline-block align-middle">
             <img
-              src="/openebx-logo.png"
+              src="/ebx-pool-logo.png"
               alt=""
               className="mx-auto mb-4 block h-[200px] w-[200px]"
             />
             <div className="hidden dark:block">
               <img
-                src="/openebx-text-white.png"
-                alt="OpenEBX"
+                src="/ebx-pool-text-white.png"
+                alt="EBX Pool"
                 className="mx-auto block h-[50px]"
               />
             </div>
             <div className="block dark:hidden">
               <img
-                src="/openebx-text-black.png"
-                alt="OpenEBX"
+                src="/ebx-pool-text-black.png"
+                alt="EBX Pool"
                 className="mx-auto block h-[50px]"
               />
             </div>
             <div className="mb-4 text-center text-black dark:text-white">
-            Open-source implementation of EBX blockchain.
+              Mining pool software for EBX blockchain.
               <br />
               <br />
               <a
-                href="https://github.com/openebx/openebx"
+                href="https://github.com/earthbucks/openebx"
                 target="_blank"
                 className="underline"
               >
                 GitHub
               </a>{' '}
-              &middot;{' '}
-              <a
-                href="https://x.com/openebx"
-                target="_blank"
-                className="underline"
-              >
-                X (Twitter)
-              </a>
-              <br />
-              <br />
-              Sponsored by{' '}
+              | Sponsored by{' '}
               <a
                 href="https://earthbucks.com"
                 target="_blank"
