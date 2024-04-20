ff947f241693671a72d0e9b904f6235280effbdb
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-20 11:45:37 -0500

    organize custom types

--- ts/earthbucks-db/src/schema-builder.ts
+++ ts/earthbucks-db/src/schema-builder.ts
@@ -17,40 +17,7 @@ import {
   binary,
 } from 'drizzle-orm/mysql-core'
 import { sql } from 'drizzle-orm'
-
-// Drizzle does not support MySQL BLOB types by default, so we have to define
-// our own custom types
-
-export const customTinyBlob = customType<{ data: Buffer }>({
-  // up to 255 bytes
-  dataType() {
-    return 'TINYBLOB'
-  },
-  toDriver(data) {
-    return data
-  },
-})
-
-export const customBlob = customType<{ data: Buffer }>({
-  // up to 64KB
-  dataType() {
-    return 'BLOB'
-  },
-})
-
-export const customMediumBlob = customType<{ data: Buffer }>({
-  // up to 16MB
-  dataType() {
-    return 'MEDIUMBLOB'
-  },
-})
-
-export const customLongBlob = customType<{ data: Buffer }>({
-  // up to 4GB
-  dataType() {
-    return 'LONGBLOB'
-  },
-})
+import { customLongBlob } from './custom-types'
 
 export const BuilderHeader = mysqlTable(
   'builder_header',
