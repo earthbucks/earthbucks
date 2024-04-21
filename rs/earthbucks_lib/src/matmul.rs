66ab5fbe6f56141e93804dbacca508e9246bbf7b
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-21 10:03:06 -0500

    psueodrandom is slower than blake3 ... so far
    test results are disappointing. i need to make this test matmul dependent, not
    just hashing or copying memory. more testing needed.
    
    running 2 tests
    Time elapsed in matmul_1024_buf is: 70.841833ms
    Time elapsed in matrix.dot(&vector) is: 4.320292ms
    test matmul::tests::test_matmul_1024 ... ok
    Time elapsed in generate_pseudo_random_1024x1024_u32_array_matmul is: 279.221584ms
    Time elapsed in matrix.dot(&vector) is: 4.169084ms
    test matmul::tests::test_generate_pseudo_random_1024x1024_u32_array_matmul_buf ... ok

--- rs/earthbucks_lib/src/matmul.rs
+++ rs/earthbucks_lib/src/matmul.rs
@@ -1,7 +1,12 @@
+// use std::vec;
 use crate::blake3::blake3_hash;
 use crate::buffer_writer::BufferWriter;
 use ndarray::{Array1, Array2};
+// use rand::distributions::Standard;
 // use std::time::Instant;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
 
 pub struct Matmul {
     source: [u8; 32],
@@ -10,6 +15,58 @@ pub struct Matmul {
 impl Matmul {
     pub fn new(source: [u8; 32]) -> Self {
         Matmul { source }
+    }
+
+    pub fn generate_pseudo_random_u8_vec(&self, len: usize) -> Vec<u8> {
+        let mut rng = StdRng::from_seed(self.source);
+        let mut vec = Vec::new();
+        for _ in 0..len {
+            vec.push(rng.gen::<u8>());
+        }
+        vec
+    }
+
+    pub fn generate_psuedo_random_u32_vec(&self, len: usize) -> Vec<u32> {
+        let vec = self.generate_pseudo_random_u8_vec(len);
+        vec.iter().map(|x| *x as u32).collect::<Vec<u32>>()
+    }
+
+    pub fn generate_pseudo_random_1024_u32_array(&self) -> Array1<u32> {
+        let vec = self.generate_psuedo_random_u32_vec(1024);
+        Array1::from_vec(vec)
+    }
+
+    pub fn generate_pseudo_random_1024x1024_u32_array(&self) -> Array2<u32> {
+        let vec = self.generate_psuedo_random_u32_vec(1024 * 1024);
+        Array2::from_shape_vec((1024, 1024), vec).unwrap()
+    }
+
+    pub fn generate_pseudo_random_1024x1024_u32_array_matmul(&self) -> Array2<u32> {
+        let start = std::time::Instant::now();
+        let matrix = self.generate_pseudo_random_1024x1024_u32_array();
+        let vector = self.generate_pseudo_random_1024_u32_array();
+        let duration = start.elapsed();
+        println!(
+            "Time elapsed in generate_pseudo_random_1024x1024_u32_array_matmul is: {:?}",
+            duration
+        );
+
+        let start = std::time::Instant::now();
+        let result = matrix.dot(&vector);
+        let duration = start.elapsed();
+        println!("Time elapsed in matrix.dot(&vector) is: {:?}", duration);
+
+        result.into_shape((1024, 1)).unwrap()
+    }
+
+    pub fn generate_pseudo_random_1024x1024_u32_array_matmul_buf(&self) -> [u8; 4096] {
+        let arr = self.generate_pseudo_random_1024x1024_u32_array_matmul();
+        let vec: Vec<u32> = arr.iter().cloned().collect();
+        let mut bw = BufferWriter::new();
+        for &value in vec.iter() {
+            bw.write_u32_be(value);
+        }
+        bw.to_u8_vec().try_into().unwrap()
     }
 
     pub fn generate_u8_vec(&self, len: usize) -> Vec<u8> {
@@ -119,15 +176,24 @@ impl Matmul {
     }
 
     pub fn matmul_1024_arr(&self) -> Array2<u32> {
+        let start = std::time::Instant::now();
         let matrix = self.gen_matrix_1024x1024();
         let vector = self.gen_vector_1024();
+        let duration = start.elapsed();
+        println!("Time elapsed in matmul_1024_buf is: {:?}", duration);
+
+        let start = std::time::Instant::now();
         let result = matrix.dot(&vector);
+        let duration = start.elapsed();
+        println!("Time elapsed in matrix.dot(&vector) is: {:?}", duration);
+
         result.into_shape((1024, 1)).unwrap()
     }
 
     pub fn matmul_1024_buf(&self) -> [u8; 4096] {
         let arr = self.matmul_1024_arr();
         let vec: Vec<u32> = arr.iter().cloned().collect();
+
         let mut bw = BufferWriter::new();
         for &value in vec.iter() {
             bw.write_u32_be(value);
@@ -139,21 +205,35 @@ impl Matmul {
 #[cfg(test)]
 mod tests {
     use super::*;
-    //use std::time::Instant;
+    // use std::time::Instant;
 
     #[test]
-    fn test_matmul_32() {
+    fn test_generate_pseudo_random_1024x1024_u32_array_matmul_buf() {
         let source = [0u8; 32];
         let matmul = Matmul { source };
 
         //let start = Instant::now();
-        let res_buf = matmul.matmul_32_buf();
+        let res_buf = matmul.generate_pseudo_random_1024x1024_u32_array_matmul_buf();
         //let duration = start.elapsed();
-        //println!("Time elapsed in matmul_1024_buf is: {:?}", duration);
+        //println!("Time elapsed in generate_pseudo_random_1024x1024_u32_array_matmul_buf is: {:?}", duration);
 
         let res_hex = hex::encode(res_buf);
-        assert_eq!(res_hex, "0007e0dd0007de0a00081c4a00078b3e00080020000a82af00086a210008a2c400070c5c00076ac40008a083000991fd000841ae0008d8210007d8f30007a43c00094a9f000846d30006f7e100079bb8000770190008b8a50007cf8a000866fe000a72d40008979b000aac220009a7720009072600088234000763ab00094c04");
+        assert_eq!(res_hex, "014a686200ff3d0b00fb510300f848fa00ff94a400f5b28e00f21f8d00f810aa00fc8d2201015e4600f23e3b00f4224e00f53ad7010355b000f2059700fdbd1100f896e700fd133601003a0200fd586300f6c7a200f74af000fd2cc900fc23a100f7bfab00fca00700f5badd00fd9cd801026efc00ff65b800fcc85c00f671ba00f62aee00f2179700f5c80a01043d9d00f0e67300fba66f00fca34600f3deb200fb254d0100927a00f9a0b700f3b94100fb331100faaaa100fdb01500f02bec00fd182a00f2deaa01024e1701018e9400f626de00f2f55300fc828a00f0428b0100fe4a00f732d300f5100100fd5f1b00f3ce5a00f7ed2800f93c1f00f7b22800f5842b00f8772700f1211800f9fa0f00f7673900fabff700f9605400f95b3e00fd758900f7212200ff09ac00f5c12800efafb000f5b75b00fbac3400f7781700ff6c1300fda9b400f3234d0101069000fc73fc00fc7c1d01003d4d00fae3940104116000ee18a100f6cbb700f8b42a01022bbd00f19baa00f9c66a00fa173b00f9c7e50103ebfe00f818e801051f4c0104a3eb00f9da1f00f92e8f00fe2dfa00fb74c500fd4e9200fe11d100f47ede01006f6d01009f5901004b4c00f148a800fa81ff00f60e9200f9403a00f82cae00f29d0000f9c28a00f43ce500f6b05a0100f6c300f4faf900f4f7e300faa22e00fa7e7700fe2f0800f384b400f79cec00fbca9c00f6df2c00f1e85400f8fa1d00f425fc00fa890e00f8fe9f00f9b72e00f7750b00fc173300f243ed00fc3fe7010015cf00f3d80c00fd925d00f5bf4b00fa973b00fa160400f7f2be00f62c4500f9654500fae92b00f571da00f4027c00f340cf00f6ee3d00f5fc6700f1c6bf00fd6e760101e7080100382e00f4e9cc00ff893d00f8640b00f789c100fc9a1800f556bd00f055d101002b7100f97ad600f04d3500f9737e00f3c5aa00f8c41900f95adf00f7dd12010664ba00fb837e00f8f9cd00f4f0db00f57ca10103d6c400fc06c60100498800f88a6b00f0bae200f50fd700f7979000f9a52200f853bc00fe388e00fbe7de00f4e57200f95bdb00fc460900f967f900fe67ed0104b0ee00f510fa00f5fa5c00f4adb400fc410a0104721300f693ea00f4dacd00feb00f0100196400fa187d00f85c4100efa81d00f6e9b900fd3dde00faaf1d0102ba5c00f7f2d90105c29000fca0db00f3c5d100fa361000f6a88d00fd0e2400f3465a00f2277800fe27b300f9a3d400f77e1700f63d9100f8909700fd8d3600eea513010238f400f6b3ad00fcb13300f3dd3601026c1b00f6206400f1962500fc129a00f4a55f00f8298300fb9bda00f38b5200f9050a00fe4eae00f675f800f2f5a200f6302f00f0a9de00f8e9a800ff095900f8492201035b6900fb032000ffa00c00f541a200fea1480104bee500ff67eb00f4530b00f97e2b00f58a3f00fac9de00f7a06600f9906400fc8e6700f3022200f0c3aa0101aa3e00f8bb0c00fd00f600ff5e2b00f516be00f8c1c000f4aa1300f7feee00f2337a00f992da00fc0f0d0100c4ba00f814eb00f8153c00fc86db00fd2a3c00f0098000f58c2500f2cd0400f25edb00f91b2c00f30ba600fc0f9a00f93a8400faf47400f1965800ff3f2a00fc038e00fbe3630104aefc00f7dd3b00f85c4e00ff79be00fdf33d00fe12a500f96d9800fb74630102e51500ffa1bf00f98a1000fe6bbe00f6d51400f061ec00fac2cd00fa4c9a00eefd150103b06200f6db0300f5167c00f45b3700f359a100fd3da100ee78b000faa95800f7e21a01041a0500ebd73d00f7cce801083e3500f3affa00fcfccd00febc2301034d6e00f88a3d00f6b89100f2822f00f250d800ff336a00f69efe00f4c38e00f7dac800fd4ef400f68bd900ff5e0100f6057700fb06e000ee7b1900f3b96200f7f58a00f43a5300f38b0500f5adb800fb2ee600f71a9600f73c590102114a00f6707b01017ea300f3adeb00fd735700f2260a01041eaa00f9c53800fb34e400ecbf7800f17cc700fa4a240101b88300f9710700fcc7e900f796a100f774c500fc64b800fd275400f45cad00f8126600f454580101cb2200fd4f8000fac6cb00f4028b00fa8135010147da00fb093400ff488f00f63f8600ee448700f9b00900fbbce400fc011f00fc50d500f4131a00f1461600ff1c3400f8f7c50105366f00fc149200fa8f8b0100771e00fddcc100facea800fcdfd200f795a900f774a700fc7b6100faa4fc00fb7a5d00fed21400f5729d00fa81de00fb6df100f43c8c00f802b400f4544900fd771b00f6744500f7d70300fdf50800f431680107340800edacb200f8730700f8733700fa225100f2b1dc00fc31af00fbe45f00f8f14200fcd63b0101228500f706110100256500ff2a3f00f9798d00fcc5da00f3f5f300f77d2001043df700fbdae200ff44f400f48a7e0105d26000fb79af00f8868400f3e3ee00fe324b00f007d000fe8f070102018100f76d8d00f80c1600f28a9300f94f8200fdf24200f456cf00f89b4b00fa8da300fae8c200fc095000f4e3b500f7352700fce81100eaff2300ff903800f5a18000f0ed3f00ee4e3a00f2cb4d00f5b38600f7944300fcb2a500fce9f400fb1e6e00f2bdaf00f67af800fa960700faf75f00fbafbf00f7b5e300f9cc8800eb203400ec6b6a00f73e9f00f98f6e00f05f2100f5179a00fff99900fd89da00f76f8300ec2d8d00fd018500f5f8bd00f31af500fc1bee00f6f4e300fac8bb00faf39200fa4bb900f2bb4600fe6a6800fa0fad00f845160105215600f928c400fd19e900f4c22900eea43b00fd824900f9329700f6be1200f6575900f748cf00f854aa00f8049000f910d100fbe4fc00fee97000f86a8200f2906d00fc32f50103606d00f6c21b00f9964800f4c80700f993cb00f653cf010172ef00f35db30100cba300fcf65f00f88ec700f2c3e400fcd34b0102251c00f7e94100f9280900f8636400f7e44000f9cbda00fc0d7e00fde47300f5fa4c00f2b9bc00f5d79400f9a4a000fad69c00fc3f0d00fc309000f963dc00febb8600fe6ec800f6412500f9154300ff7eb900f5a56e00f4da1f00f7956c00f12f59010054b500fa7b1700f4042500f7a0a800f56ee000f4516d0104765e00fba64800fb4a9f01026dd800f73b5500fdc56a00fd56f00103025700efe17c00f58eed00fb016e00efa42200fbe38400fb640500f794d800f6b3ec00f3121100febf7700f634b00103883f00f08b9100f105f000f39f9600feb89c00f0d35200fadd0001003ffd00fe239200fdc3b400f6f65c00fa380500f59f6800feef4b00f4401c00fc1a0f00f6a96400ffadb300fd72fe00ff772600fd4f0e00fb6b6c00ffcb6500fb0d4d00f807c400f5d69f00f711c800f803ef00f7530400fd2d0000f55cac00fe00a200f82aa400faa20b0102f4a500ff3f0700f63a5200fab95200fa3c9800f7dce500fd4b4700fa319700f9257100fa689d00fe3a4901026be200fc38e200f2e26400f5bbf400faec1f00fb073a00f0ff5000fa481500ffc4e700fae89700f7054c00f728ba00fba74600eee28f00f90f5a00f3a4f800f46fb100f7a54f00face8900fdf35100f1079700eee94800f51c5000f9a5a900f9632c00fed30000fa1c1800f6a87b010525cc00fa0d6500f885b501036c7900fdd15401017b6100f8834700f1818000fa73e600f8394d00fdc8a300fddcfb00fb1e57010411b600f7338500fbfb5500feff1400f86dfc010441a600f888af0100241500fd078c00f6abe900f9ee1a00fb489600f20f0e00f3f57300edaab400f96d9900f39179010288b400f2098400fc29bf00f769320103b9c700f78f9c00f23da500ff0a5700fc566400f8c9f600f8569100fe57c700f99ac10101bc9d00fe74b901044c3400f9c9a700ee099b00f90acc00f3e68401004b4100f3b80700f6757600fd474c00fc3f5a00f783ed00fed8d700ee730900fbffa40103663100f41e5b00fc00d600f7f48700fc6d1500f4fdca00f674f500fdc4140100edcd00f6c4cb00fa36ce00f2fd4100f8f05600ffd25200f1914a00fd6a110102b43e00f5455a00f3689000f73b2500fa54b500f71f1400f61dff00fa333800fbacc800f6734900f1b87900f7564e00f11aff00ef3a1400ff742100f513b400fae12f00f23f400105a1e200f7fef300fcae6200f6afa6010336ba00eb9f7e00f4f98b00fbba3f00fd6f9600fbe01200f7101400f5ce5300f82a7c00f258b800fd081400fc5edd00fcb13d01001f9f00f65e0200f6930700fa7b9e00fcaa3500fb425300f55ec000f6f5eb00fd89360103660900fe087400f5432e00f85a3800f92a8100f2ba0b00f4945200fc28580102830401026d7000ffa54500f43f5600fb79e200f7dff10101820400f3a86200fbaa520100706f00f675ff00fa3f9b00fc023100f6c91600f99ca500f72f1e00fa4c6f00fa7c8200f0326a00f74d8101012df80105432400f976f700f830df00f46f8100f6ff8901009e330100506c00fdca0f00f4f0ff00f8c3eb00ff858600f83eee00fa4ad700fe4f4700f6d7da00fdf5da00f7428600f5907c01071b3901015a3800f705bf00f8920101018ba500f0781400fbdc2e00f85d0d00f91e5800fec4de0100a30b00f5d4a300fa0b4200f3cfd500fd2e2400fcde9500fd547200fb5d7b00fa492d00f7ac2c00f85c5e010095ad00f53a4100f6c0a30100e4bb00f7b3b400f3d44000f65ab100f9c17900f5335700ebe84800f554d100fad5f000fc627f00f6338c00f29d1a0102636400f5f36c00fb305601016d5700fedf4a01074beb00f7570b00ffd6c200fabd6a00f85cdf01001d3300fb2438010166d700f5c90300f3dca300f8d57301004cc800fc7d5f00fe6b1c00fc54d000f1bc4100ff3a2400ee82b601005f1b00f75f7a00faadbf00fdbb1a00fe6b4100f881c700f8cca800fb2b6a00f6674400fec4bc00fa43b900f88b0100f2c0d600f95f2100f744cd00fc21f500f4494000fcd5470100b57300f8499800fc7bd800fac39200fe715d00f6fea000fdc6eb00f827e900f5fc4300fd682200fa860300ff27a000fe4ae20101befa00fa220900fedb3c00f11b15010195fd00f7b24d010578cb00fa784500f96269010116470102d5310102510e00f99a7000ee865e00fc9e6c00fb748000fb2d0600f3748600fdb87100fe3fbc00f4e57a00fb5b5900f3f5c800f9b79100fd1c5a00f3d39800f7ade800f7c81d00f9796c00fda59800f56fb300f57d7100fc0f6c00fe7dbf00f9b5f90101f60900fb6aad00fc3c4200f1563900eac3ea00f47ec500f2f13f00f5f25200f590f900f929990100fca700f5c9a100fa420400fbff8600fc6e0400f7c4b700fa707d00fb3ef700fde59200f51df900f85ddb00f68cb400f97d6600f56fd400f7713200fc272100f9c7b300fa932100f680bd00f2b5fb00f76b6400fbb5e700fe532c00ef129200f6639200fc7e2800fd685400f5e96800f7ab1800fa773400f988f100f957100101866700f2625a00fee65c0101221f00f7162c00faecbc00f51b6a00fd03c700f91c7b00fa056200fa6a28010140c900fe751400f2732e00fae96100ff9a360101225b00f7cd8b00fa31d800f0637d00f8b95a00f92a2d00fb877400fcd93d00fbfdc100fd89fd00eeb4cb00f6a9f900fa003200f776fc00edbd5800f6877500fbade10104aa4e00f4364d00f3bb1000f5d93f00fdaf7700ffa61600f14117");
     }
+
+    // #[test]
+    // fn test_matmul_32() {
+    //     let source = [0u8; 32];
+    //     let matmul = Matmul { source };
+
+    //     //let start = Instant::now();
+    //     let res_buf = matmul.matmul_32_buf();
+    //     //let duration = start.elapsed();
+    //     //println!("Time elapsed in matmul_1024_buf is: {:?}", duration);
+
+    //     let res_hex = hex::encode(res_buf);
+    //     assert_eq!(res_hex, "0007e0dd0007de0a00081c4a00078b3e00080020000a82af00086a210008a2c400070c5c00076ac40008a083000991fd000841ae0008d8210007d8f30007a43c00094a9f000846d30006f7e100079bb8000770190008b8a50007cf8a000866fe000a72d40008979b000aac220009a7720009072600088234000763ab00094c04");
+    // }
 
     #[test]
     fn test_matmul_1024() {
