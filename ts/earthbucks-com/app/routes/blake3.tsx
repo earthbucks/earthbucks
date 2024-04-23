70816246e4fc9420bc3ff2913111ad70fdbc8e86
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-23 03:24:40 -0500

    copying hash is a lot faster
    takeaway: reducing the number of hashes to create the matrix makes a big
    difference. even though i am using wasm for blake3 in the browser, it still
    saves a significant amount of time by not making that many hashes.
    
    the alternate strategy is to just copy the first hash. subsequent hashes
    theoretically provide no additional pseudorandomness, so why not just use a
    single hash? not only is this faster, but this may allow me to use the pure js
    version of the hash, saving a lot of hassle getting blake3 in the browser.
    
    by using a single hash, the time to produce a large matrix is cut down to about
    1/10. instead, most of the remaining time is just the time it takes to send the
    matrix to the GPU.
    
    takeaway: we should copy the first hash to save CPU.
    
    replicating hash over and over:
    
    create 2048 matrix arr: 18.278076171875 ms
    create 2048 matrix: 54.05810546875 ms
    matcube2048: 209.045166015625 ms
    
    hashing the hash over and over:
    
    create 2048 matrix arr: 141.740966796875 ms
    create 2048 matrix: 162.233154296875 ms
    matcube2048: 313.6162109375 ms

--- ts/earthbucks-com/app/routes/blake3.tsx
+++ ts/earthbucks-com/app/routes/blake3.tsx
@@ -20,17 +20,48 @@ class Matmul {
     this.seed = seed;
     this.blake3Hash = blake3Hash;
   }
-
-  async createBinaryMatrix(size: number): Promise<tf.Tensor> {
+  async createBinaryMatrixArr(size: number): Promise<number[]> {
     let matrixData: number[] = [];
-    let currentHash = this.blake3Hash(Buffer.from(this.seed));
+    let currentHash = this.blake3Hash(this.seed);
     let hashIter = currentHash.values();
 
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) {
         let byte = hashIter.next().value;
         if (byte === undefined) {
-          currentHash = this.blake3Hash(currentHash);
+          currentHash = Buffer.from(currentHash);
+          hashIter = currentHash.values();
+          byte = hashIter.next().value;
+        }
+        for (let bit = 7; bit >= 0; bit--) {
+          let value = (byte >> bit) & 1;
+          matrixData.push(value);
+          if (matrixData.length >= size * size) {
+            break;
+          }
+        }
+        if (matrixData.length >= size * size) {
+          break;
+        }
+      }
+      if (matrixData.length >= size * size) {
+        break;
+      }
+    }
+
+    return matrixData
+  }
+
+  async createBinaryMatrix(size: number): Promise<tf.Tensor> {
+    let matrixData: number[] = [];
+    let currentHash = this.blake3Hash(this.seed);
+    let hashIter = currentHash.values();
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        let byte = hashIter.next().value;
+        if (byte === undefined) {
+          currentHash = Buffer.from(currentHash);
           hashIter = currentHash.values();
           byte = hashIter.next().value;
         }
@@ -56,6 +87,10 @@ class Matmul {
 
   async squareMatrix(matrix: tf.Tensor): Promise<tf.Tensor> {
     return tf.matMul(matrix, matrix);
+  }
+
+  async cubeMatrix(matrix: tf.Tensor): Promise<tf.Tensor> {
+    return tf.matMul(matrix, await this.squareMatrix(matrix));
   }
 
   async matmul256(): Promise<Buffer> {
@@ -113,6 +148,76 @@ class Matmul {
 
     return this.blake3Hash(Buffer.from(squaredBufU8));
   }
+
+  async matcube256(): Promise<Buffer> {
+    let matrix = await this.createBinaryMatrix(256);
+    let cubed = await this.cubeMatrix(matrix);
+    let cubedBufU16 = cubed.dataSync();
+    let cubedBufU8: number[] = [];
+
+    for (let x of cubedBufU16) {
+      cubedBufU8.push(x & 0xff);
+      cubedBufU8.push(x >> 8);
+    }
+
+    return this.blake3Hash(Buffer.from(cubedBufU8));
+  }
+
+  async matcube400(): Promise<Buffer> {
+    let matrix = await this.createBinaryMatrix(400);
+    let cubed = await this.cubeMatrix(matrix);
+    let cubedBufU16 = cubed.dataSync();
+    let cubedBufU8: number[] = [];
+
+    for (let x of cubedBufU16) {
+      cubedBufU8.push(x & 0xff);
+      cubedBufU8.push(x >> 8);
+    }
+
+    return this.blake3Hash(Buffer.from(cubedBufU8));
+  }
+
+  async matcube512(): Promise<Buffer> {
+    let matrix = await this.createBinaryMatrix(512);
+    let cubed = await this.cubeMatrix(matrix);
+    let cubedBufU16 = cubed.dataSync();
+    let cubedBufU8: number[] = [];
+
+    for (let x of cubedBufU16) {
+      cubedBufU8.push(x & 0xff);
+      cubedBufU8.push(x >> 8);
+    }
+
+    return this.blake3Hash(Buffer.from(cubedBufU8));
+  }
+
+  async matcube1024(): Promise<Buffer> {
+    let matrix = await this.createBinaryMatrix(1024);
+    let cubed = await this.cubeMatrix(matrix);
+    let cubedBufU16 = cubed.dataSync();
+    let cubedBufU8: number[] = [];
+
+    for (let x of cubedBufU16) {
+      cubedBufU8.push(x & 0xff);
+      cubedBufU8.push(x >> 8);
+    }
+
+    return this.blake3Hash(Buffer.from(cubedBufU8));
+  }
+
+  async matcube2048(): Promise<Buffer> {
+    let matrix = await this.createBinaryMatrix(2048);
+    let cubed = await this.cubeMatrix(matrix);
+    let cubedBufU16 = cubed.dataSync();
+    let cubedBufU8: number[] = [];
+
+    for (let x of cubedBufU16) {
+      cubedBufU8.push(x & 0xff);
+      cubedBufU8.push(x >> 8);
+    }
+
+    return this.blake3Hash(Buffer.from(cubedBufU8));
+  }
 }
 
 export const meta: MetaFunction = () => {
@@ -131,20 +236,108 @@ export default function Landing() {
   } else {
     // running in a browser environment
     import("blake3/browser").then(async ({ createHash, hash: blake3Hash }) => {
+      function delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      await delay(1000);
       let browserBlake3Hash = (data: Buffer) => {
         const hasher = createHash();
         hasher.update(data);
         return Buffer.from(hasher.digest());
       };
-      let seed = Buffer.from("seed");
-      let matmul = new Matmul(seed, browserBlake3Hash);
-      console.time("create 1024 matrix")
-      let matrix = await matmul.createBinaryMatrix(1024);
-      console.timeEnd("create 1024 matrix")
-      console.time("matmul1024");
-      let res = await matmul.matmul1024();
-      console.timeEnd("matmul1024");
-      console.log(res.toString("hex"));
+      console.log('begin')
+      // matsquare1024
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 1024 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(1024);
+        console.timeEnd("create 1024 matrix arr");
+        console.time("create 1024 matrix");
+        let matrix = await matmul.createBinaryMatrix(1024);
+        console.timeEnd("create 1024 matrix");
+        console.time("matmul1024");
+        let res = await matmul.matmul1024();
+        console.timeEnd("matmul1024");
+        // console.log(res.toString("hex"));
+      }
+      // matcube256
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 256 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(256);
+        console.timeEnd("create 256 matrix arr");
+        console.time("create 256 matrix");
+        let matrix = await matmul.createBinaryMatrix(256);
+        console.timeEnd("create 256 matrix");
+        console.time("matcube256");
+        let res = await matmul.matcube256();
+        console.timeEnd("matcube256");
+        // console.log(res.toString("hex"));
+      }
+      // matcube400
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 400 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(400);
+        console.timeEnd("create 400 matrix arr");
+        console.time("create 400 matrix");
+        let matrix = await matmul.createBinaryMatrix(400);
+        console.timeEnd("create 400 matrix");
+        console.time("matcube400");
+        let res = await matmul.matcube400();
+        console.timeEnd("matcube400");
+        // console.log(res.toString("hex"));
+      }
+      // matcube512
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 512 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(512);
+        console.timeEnd("create 512 matrix arr");
+        console.time("create 512 matrix");
+        let matrix = await matmul.createBinaryMatrix(512);
+        console.timeEnd("create 512 matrix");
+        console.time("matcube512");
+        let res = await matmul.matcube512();
+        console.timeEnd("matcube512");
+        // console.log(res.toString("hex"));
+      }
+      // matcube1024
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 1024 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(1024);
+        console.timeEnd("create 1024 matrix arr");
+        console.time("create 1024 matrix");
+        let matrix = await matmul.createBinaryMatrix(1024);
+        console.timeEnd("create 1024 matrix");
+        console.time("matcube1024");
+        let res = await matmul.matcube1024();
+        console.timeEnd("matcube1024");
+        // console.log(res.toString("hex"));
+      }
+      // matcube2048
+      {
+        let seed = Buffer.from("seed");
+        let matmul = new Matmul(seed, browserBlake3Hash);
+        console.time("create 2048 matrix arr");
+        let matrixn = await matmul.createBinaryMatrixArr(2048);
+        console.timeEnd("create 2048 matrix arr");
+        console.time("create 2048 matrix");
+        let matrix = await matmul.createBinaryMatrix(2048);
+        console.timeEnd("create 2048 matrix");
+        console.time("matcube2048");
+        let res = await matmul.matcube2048();
+        console.timeEnd("matcube2048");
+        // console.log(res.toString("hex"));
+      }
+      console.log('end')
+
     });
   }
   return (
