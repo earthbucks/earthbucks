45a1a6151e909b4bb6d2a26d05d1a62b2b5ea8e6
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-30 09:07:14 -0500

    add icons. format keys for sign in.

--- ts/compubutton-com/app/routes/_auth.signin.tsx
+++ ts/compubutton-com/app/routes/_auth.signin.tsx
@@ -18,22 +18,44 @@ export default function Landing() {
       <div className="my-4 text-black dark:text-white">
         <p>
           Please save your key pair in localStorage (client-side browser
-          storage) to sign in. (New here? <Link to="/register" className='underline'>Register first</Link>.)
+          storage) to sign in. (New here?{" "}
+          <Link to="/register" className="underline">
+            Register first
+          </Link>
+          .)
         </p>
       </div>
       <div className="my-4">
-        <div className="my-2">
+        <div className="relative my-2">
+          <label htmlFor="public-key">
+            <img
+              src="/sun-128.png"
+              alt="Sun"
+              draggable="false"
+              className="absolute left-[9px] top-[9px] h-[24px] w-[24px]"
+            />
+          </label>
           <input
+            id="public-key"
             type="text"
             placeholder="Public Key"
-            className="w-full flex-grow overflow-hidden rounded-full border-[2px] border-gray-700 bg-white p-2 text-gray-600 dark:border-gray-300 dark:bg-black dark:text-gray-400"
+            className="w-full flex-grow overflow-hidden rounded-full border-[1px] border-gray-700 bg-white p-2 pl-[34px] text-gray-600 focus:outline focus:outline-2 focus:outline-primary-blue-500 dark:border-gray-300 dark:bg-black dark:text-gray-400"
           />
         </div>
-        <div className="my-2">
+        <div className="relative my-2">
+          <label htmlFor="private-key">
+            <img
+              src="/black-button-128.png"
+              alt="Sun"
+              draggable="false"
+              className="absolute left-[9px] top-[9px] h-[24px] w-[24px]"
+            />
+          </label>
           <input
             type="password"
+            id="private-key"
             placeholder="Private Key"
-            className="w-full flex-grow overflow-hidden rounded-full border-[2px] border-gray-700 bg-white p-2 text-gray-600 dark:border-gray-300 dark:bg-black dark:text-gray-400"
+            className="w-full flex-grow overflow-hidden rounded-full border-[1px] border-gray-700 bg-white p-2 pl-[34px] text-gray-600 focus:outline focus:outline-2 focus:outline-primary-blue-500 dark:border-gray-300 dark:bg-black dark:text-gray-400"
           />
         </div>
       </div>
