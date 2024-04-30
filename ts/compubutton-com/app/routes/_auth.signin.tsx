6348353b1bb41002fef42e15cfa3d33dc905fde0
Ryan X. Charles <ryan@ryanxcharles.com>
2024-04-30 06:52:35 -0500

    pub key / priv key input box

--- ts/compubutton-com/app/routes/_auth.signin.tsx
+++ ts/compubutton-com/app/routes/_auth.signin.tsx
@@ -3,6 +3,7 @@ import Button from "../button";
 import { Buffer } from "buffer";
 import { blake3PowAsync, blake3Sync } from "earthbucks-blake3/src/blake3-async";
 import Footer from "~/components/footer";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,10 +16,32 @@ export default function Landing() {
   return (
     <div className="mx-auto max-w-[400px]">
       <div className="my-4 text-black dark:text-white">
-        <p>Please save your key pair in localStorage (client-side browser storage) to sign in.</p>
+        <p>
+          Please save your key pair in localStorage (client-side browser
+          storage) to sign in. (New here? <Link to="/register" className='underline'>Register first</Link>.)
+        </p>
       </div>
-      <div className="mx-auto w-[320px]">
+      <div className="my-4">
+        <div className="my-2">
+          <input
+            type="text"
+            placeholder="Public Key"
+            className="w-full flex-grow overflow-hidden rounded-full border-[2px] border-gray-700 bg-white p-2 text-gray-600  dark:border-gray-300 dark:bg-black dark:text-gray-400"
+          />
+        </div>
+        <div className="my-2">
+          <input
+            type="password"
+            placeholder="Private Key"
+            className="w-full flex-grow overflow-hidden rounded-full border-[2px] border-gray-700 bg-white p-2 text-gray-600  dark:border-gray-300 dark:bg-black dark:text-gray-400"
+          />
+        </div>
+      </div>
+      <div className="mx-auto my-4 w-[320px]">
         <Button initialText="Save" disabled />
+      </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Sign in" mode="secret" disabled />
       </div>
     </div>
   );
