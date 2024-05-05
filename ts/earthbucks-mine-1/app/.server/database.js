ac388c84d7dd80884398e43ae12ee8a158f5da37
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-05 08:30:01 -0500

    earthbucks-mine-1

---
+++ ts/earthbucks-mine-1/app/.server/database.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+// Create the connection to the database
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+// simple query
+connection.query("show tables", function (err, results, fields) {
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra metadata about results, if available
+});
+
+// Example with placeholders
+connection.query(
+  "select 1 from dual where ? = ?",
+  [1, 1],
+  function (err, results) {
+    console.log(results);
+  },
+);
+
+connection.end();
---
+++ ts/earthbucks-mine-1/app/routes/_index.tsx
@@ -0,0 +1,79 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "EarthBucks Mine 1" },
+    { name: "description", content: "Welcome to EarthBucks!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="">
+      <div className="mb-4 mt-4 flex">
+        <div className="mx-auto">
+          <div className="inline-block align-middle">
+            <img
+              src="/earthbucks-coin.png"
+              alt=""
+              className="mx-auto mb-4 block h-[200px] w-[200px]"
+            />
+            <div className="hidden dark:block">
+              <img
+                src="/earthbucks-text-white.png"
+                alt="EarthBucks"
+                className="mx-auto block h-[50px]"
+              />
+            </div>
+            <div className="block dark:hidden">
+              <img
+                src="/earthbucks-text-black.png"
+                alt="EarthBucks"
+                className="mx-auto block h-[50px]"
+              />
+            </div>
+            <div className="mb-4 text-center text-black dark:text-white">
+              42 trillion EBX. No pre-mine. GPUs. Big blocks. Script.
+              <br />
+              <br />
+              Genesis block in 2024.
+              <br />
+              <br />
+              <a
+                href="https://t.me/+e-PoBNNgdDU3MDUx"
+                target="_blank"
+                className="underline"
+              >
+                Telegram
+              </a>{" "}
+              &middot;{" "}
+              <a
+                href="https://discord.gg/dZfyrFk6uh"
+                target="_blank"
+                className="underline"
+              >
+                Discord
+              </a>{" "}
+              &middot;{" "}
+              <a
+                href="https://x.com/earthbucks_com"
+                target="_blank"
+                className="underline"
+              >
+                X (Twitter)
+              </a>{" "}
+              &middot;{" "}
+              <a
+                href="https://github.com/earthbucks/earthbucks"
+                target="_blank"
+                className="underline"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
