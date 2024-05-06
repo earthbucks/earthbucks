deb02d7de7c4d6fec422311546a5df34e835c5be
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-06 04:50:19 -0500

    unused code

--- ts/earthbucks-mine-1/app/routes/_index.tsx
+++ ts/earthbucks-mine-1/app/routes/_index.tsx
@@ -4,13 +4,8 @@ import type {
   LoaderFunctionArgs,
   MetaFunction,
 } from "@remix-run/node";
-import { useNavigate } from "@remix-run/react";
-import Button from "~/button";
 import Footer from "~/components/footer";
-import { Buffer } from "buffer";
-import { blake3PowAsync, blake3Sync } from "earthbucks-blake3/src/blake3-async";
 import Header from "~/components/header";
-import { $path } from "remix-routes";
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,16 +15,6 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Landing() {
-  const navigate = useNavigate();
-
-  async function onSignin() {
-    navigate($path("/signin"));
-  }
-
-  async function onRegister() {
-    navigate($path("/new"));
-  }
-
   return (
     <div className="">
       <Header />
