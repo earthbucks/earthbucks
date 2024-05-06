c670e045c55b3523c44cca89871d6fcf3b0f1895
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-06 12:56:00 -0500

    button mode 42

--- ts/earthbucks-mine-1/app/routes/buttons.tsx
+++ ts/earthbucks-mine-1/app/routes/buttons.tsx
@@ -4,7 +4,7 @@ import { $image } from "~/util";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "EarthBucks Mine 1" },
+    { title: "Button Modes Demonstration | EarthBucks Mine 1" },
     { name: "description", content: "Welcome to EarthBucks Mine 1!" },
   ];
 };
@@ -12,68 +12,29 @@ export const meta: MetaFunction = () => {
 export default function Landing() {
   return (
     <div className="">
-      <div className="mb-4 mt-4 flex">
-        <div className="mx-auto">
-          <div className="inline-block align-middle">
-            <img
-              src={$image("/button-logo.png")}
-              alt=""
-              className="mx-auto mb-4 block aspect-square w-[120px] rounded-full bg-[#020a2c] p-[1px] shadow-lg shadow-[#04408d]"
-            />
-            <div className="hidden dark:block">
-              <img
-                src={$image("/compubutton-text-white.png")}
-                alt="EarthBucks Mine 1"
-                className="mx-auto block w-[300px]"
-              />
-            </div>
-            <div className="block dark:hidden">
-              <img
-                src={$image("/compubutton-text-black.png")}
-                alt="EarthBucks Mine 1"
-                className="mx-auto block w-[300px]"
-              />
-            </div>
-            <div className="mt-4 text-center text-black dark:text-white">
-              Button modes demonstration.
-            </div>
-          </div>
-        </div>
+      <div className="mx-auto my-4 text-center text-black dark:text-white">
+        Button modes demonstration.
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Standard" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Standard" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="EarthBucks" mode="pay" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="EarthBucks" mode="pay" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Computcha" mode="computcha" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Computcha" mode="computcha" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Computcha" mode="mine" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Computcha" mode="mine" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Compusecret" mode="secret" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Compusecret" mode="secret" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Compucredits" mode="credits" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Compucredits" mode="credits" />
       </div>
-      <div className="mb-4 mt-4 h-[80px]">
-        <div className="mx-auto w-[320px]">
-          <Button initialText="Artintellica" mode="ai" />
-        </div>
+      <div className="mx-auto my-4 w-[320px]">
+        <Button initialText="Artintellica" mode="ai" />
       </div>
     </div>
   );
