fbf80527a316a188d547fbda1870528252f870b1
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-08 11:28:31 -0500

    display blog post. not formatted yet.

---
+++ ts/earthbucks-com/app/routes/blog.$filename.tsx
@@ -0,0 +1,84 @@
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import Logo from "~/components/logo";
+import blogPosts from "~/blog/index.json";
+import { useLoaderData } from "@remix-run/react";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+interface BlogPost {
+  title: string;
+  date: string;
+  author: string;
+  filename: string;
+  content: string;
+}
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const filename = params.filename;
+  const newBlogPosts: BlogPost[] = blogPosts
+    .map((post) => {
+      return {
+        title: post.title,
+        date: post.date,
+        author: post.author,
+        filename: post.filename,
+        content: "",
+      };
+    })
+    .sort((a, b) => a.date.localeCompare(b.date))
+    .reverse();
+
+  const blogPost = newBlogPosts.find(
+    (post) => post.filename === `${filename}.md`,
+  );
+  if (!blogPost) {
+    throw new Response("Not found", { status: 404 });
+  }
+
+  const blogDir = path.join(__dirname, "..", "blog");
+  const filePath = path.join(blogDir, `${filename}.md`);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const content = fileContent.split("+++")[2]?.trim() || "";
+  blogPost.content = content;
+
+  return json({ blogPost });
+}
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Blog | EarthBucks" },
+    { name: "description", content: "Welcome to EarthBucks!" },
+  ];
+};
+
+export default function BlogIndex() {
+  let loaderData = useLoaderData<typeof loader>();
+  let blogPost = loaderData.blogPost;
+  return (
+    <div>
+      <div className="mx-auto my-4">
+        <Logo />
+      </div>
+      <div className="mx-auto my-4 max-w-[400px]">
+        <div>
+          <h1 className="my-4 text-center text-2xl font-bold text-black dark:text-white">
+            {blogPost.title}
+          </h1>
+          <div className="my-4 text-center text-sm text-gray-600 dark:text-gray-400">
+            {blogPost.date} &middot; {blogPost.author}
+          </div>
+          <div className="text-black dark:text-white">{blogPost.content}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ ts/earthbucks-com/app/routes/blog._index.tsx
@@ -0,0 +1,73 @@
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import Logo from "~/components/logo";
+import blogPosts from "~/blog/index.json";
+import { useLoaderData } from "@remix-run/react";
+
+interface BlogPost {
+  title: string;
+  date: string;
+  author: string;
+  filename: string;
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const newBlogPosts: BlogPost[] = blogPosts
+    .map((post) => {
+      return {
+        title: post.title,
+        date: post.date,
+        author: post.author,
+        filename: post.filename,
+      };
+    })
+    .sort((a, b) => a.date.localeCompare(b.date))
+    .reverse();
+  return json({ blogPosts: newBlogPosts });
+}
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Blog | EarthBucks" },
+    { name: "description", content: "Welcome to EarthBucks!" },
+  ];
+};
+
+export default function BlogIndex() {
+  let loaderData = useLoaderData<typeof loader>();
+  let blogPosts = loaderData.blogPosts;
+  return (
+    <div>
+      <div className="mx-auto my-4">
+        <Logo />
+      </div>
+      <div className="mx-auto my-4 max-w-[400px]">
+        <div>
+          <h1 className="my-4 text-center text-2xl font-bold text-black dark:text-white">
+            Blog
+          </h1>
+          <div>
+            <div className="mb-4 text-black dark:text-white">
+              {blogPosts.map((post) => (
+                <div key={post.filename} className="mb-4">
+                  <a
+                    href={`/blog/${post.filename.replace(".md", "")}`}
+                    className="text-lg font-semibold leading-3 underline"
+                  >
+                    {post.title}
+                  </a>
+                  <div className="text-sm text-gray-600 dark:text-gray-400">
+                    {post.date} &middot; {post.author}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
