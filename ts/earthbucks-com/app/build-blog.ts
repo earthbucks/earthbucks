878739978a8aec2d70cba17e4eaffe76b961476d
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-09 05:58:16 -0500

    remove obsolete comments

--- ts/earthbucks-com/app/build-blog.ts
+++ ts/earthbucks-com/app/build-blog.ts
@@ -2,41 +2,33 @@ import fs from "fs";
 import path from "path";
 import toml from "toml";
 
-// Define the type for a blog post
 interface BlogPost {
   title: string;
   author: string;
   date: string;
-  // content: string;
   filename: string;
 }
 
-// Get all markdown files in the docs/blog directory
 const blogDir = path.join("app", "blog");
 const filenames = fs
   .readdirSync(blogDir)
   .filter((filename) => filename.endsWith(".md"));
 
-// Parse each file and add it to the blogPosts array
 const blogPosts: BlogPost[] = filenames.map((filename) => {
   const filePath = path.join(blogDir, filename);
   const fileContent = fs.readFileSync(filePath, "utf8");
 
-  // Manually extract and parse the frontmatter
   const frontmatterDelimiter = "+++";
   const splitContent = fileContent.split(frontmatterDelimiter);
   const frontmatter = toml.parse(splitContent[1] as string);
-  // const content = splitContent.slice(2).join(frontmatterDelimiter).trim();
 
   return {
     filename,
     title: frontmatter.title as string,
     author: frontmatter.author as string,
     date: frontmatter.date as string,
-    // content,
   };
 });
 
-// Write the blogPosts array to a JSON file
 const jsonPath = path.join("app", "blog", "index.json");
 fs.writeFileSync(jsonPath, JSON.stringify(blogPosts, null, 2));
