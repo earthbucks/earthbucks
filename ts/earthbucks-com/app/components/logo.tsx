7c5eba94828708e37a53104a8b6edf99c1dd8201
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-10 04:47:54 -0500

    earthbucks wallet

--- ts/earthbucks-com/app/components/logo.tsx
+++ ts/earthbucks-com/app/components/logo.tsx
@@ -2,14 +2,23 @@ import { $image } from "~/util";
 
 export default function Logo() {
   return (
-    <div className="mx-auto block aspect-square w-[120px] rounded-full bg-secondary-blue-500 p-[3px] shadow-lg shadow-[#04408d] outline outline-1 outline-black">
+    <div className="mx-auto block aspect-square w-[160px] rounded-full bg-secondary-blue-500 p-[3px] shadow-lg shadow-[#04408d] outline outline-1 outline-black">
       <div className=" rounded-full bg-[#12b3ec] p-1 shadow-[inset_5px_5px_10px_#04408d]">
         <img
-          src={$image("/earth-coin-2.png")}
+          src={$image("/earthbucks-wallet-2-big.png")}
           alt=""
-          className="block rounded-full"
+          className="block"
         />
       </div>
     </div>
   );
+  // return (
+  //   <div className="mx-auto block aspect-square w-[120px] ">
+  //     <img
+  //       src={$image("/earthbucks-wallet-2-big.png")}
+  //       alt=""
+  //       className="block"
+  //     />
+  //   </div>
+  // );
 }
