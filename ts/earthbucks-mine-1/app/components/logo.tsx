65001df31b932cd831e60697d85eaadd0649f67f
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-11 04:34:38 -0500

    change outline to bg

--- ts/earthbucks-mine-1/app/components/logo.tsx
+++ ts/earthbucks-mine-1/app/components/logo.tsx
@@ -2,13 +2,15 @@ import { $image } from "~/images";
 
 export default function Logo() {
   return (
-    <div className="mx-auto block aspect-square w-[120px] rounded-full bg-secondary-blue-500 p-[3px] shadow-lg shadow-[#04408d] outline outline-1 outline-black">
-      <div className=" rounded-full bg-[#12b3ec] p-1 shadow-[inset_5px_5px_10px_#04408d]">
-        <img
-          src={$image("/earthbucks-coin-3.png")}
-          alt=""
-          className="block rounded-full"
-        />
+    <div className="mx-auto block aspect-square w-[120px] rounded-full bg-black p-[1px]">
+      <div className="rounded-full bg-secondary-blue-500 p-[3px] shadow-lg shadow-[#04408d]">
+        <div className="rounded-full bg-[#12b3ec] p-1 shadow-[inset_5px_5px_10px_#04408d]">
+          <img
+            src={$image("/earthbucks-coin-3.png")}
+            alt=""
+            className="block rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
