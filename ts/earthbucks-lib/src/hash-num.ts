7711aef15750668f3e3d13e981eaf35f94d17074
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-12 06:24:52 -0500

    unwind hash-num try/catch

--- ts/earthbucks-lib/src/hash-num.ts
+++ ts/earthbucks-lib/src/hash-num.ts
@@ -9,31 +9,23 @@ export default class HashNum {
   }
 
   static fromIsoBuf(target: Buffer): Result<HashNum, string> {
-    try {
-      if (target.length !== 32) {
-        return new Err("Invalid target length");
-      }
-      let hex = target.toString("hex");
-      let num = BigInt("0x" + hex);
-      return new Ok(new HashNum(num));
-    } catch (err) {
-      return new Err(err?.toString() || "Unknown error parsing hash num");
+    if (target.length !== 32) {
+      return new Err("Invalid target length");
     }
+    let hex = target.toString("hex");
+    let num = BigInt("0x" + hex);
+    return new Ok(new HashNum(num));
   }
 
   toIsoBuf(): Result<Buffer, string> {
-    try {
-      let hex = this.num.toString(16);
-      // ensure length is 64 characters
-      if (hex.length > 64) {
-        return new Err("Target number is too large");
-      }
-      while (hex.length < 64) {
-        hex = "0" + hex;
-      }
-      return new Ok(Buffer.from(hex, "hex"));
-    } catch (err) {
-      return new Err(err?.toString() || "Unknown error serializing hash num");
+    let hex = this.num.toString(16);
+    // ensure length is 64 characters
+    if (hex.length > 64) {
+      return new Err("Target number is too large");
     }
+    while (hex.length < 64) {
+      hex = "0" + hex;
+    }
+    return new Ok(Buffer.from(hex, "hex"));
   }
 }
