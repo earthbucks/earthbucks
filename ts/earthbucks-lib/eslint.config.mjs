be9e34d849fe432a6950e77a1ab09a0a153d8383
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-17 15:48:08 -0500

    eslint settings for vscode

--- ts/earthbucks-lib/eslint.config.mjs
+++ ts/earthbucks-lib/eslint.config.mjs
@@ -1,16 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-//import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
 
-export default [
+export default tseslint.config(
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  //pluginReactConfig,
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "error",
     },
   },
-];
+);
