b1e7443055644a85035de460dbaeb680d688e10c
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-23 14:33:10 -0500

    ts: test: pkhxr 90d 60d expired

--- ts/earthbucks-lib/test/tx-verifier.test.ts
+++ ts/earthbucks-lib/test/tx-verifier.test.ts
@@ -595,4 +595,75 @@ describe("TxVerifier", () => {
       expect(verified).toBe(true);
     });
   });
+
+  describe.only("pkhxr 90d 60d expired", () => {
+    beforeEach(() => {
+      txOutBnMap = new TxOutBnMap();
+      pkhKeyMap = new PkhKeyMap();
+      // generate 5 keys, 5 outputs, and add them to the txOutMap
+      for (let i = 0; i < 5; i++) {
+        const key = KeyPair.fromRandom();
+        const pkh = Pkh.fromPubKeyBuf(Buffer.from(key.pubKey.toIsoBuf()));
+        pkhKeyMap.add(key, pkh.buf);
+        const script = Script.fromPkhxr90d60dOutput(pkh.buf, pkh.buf);
+        const txOut = new TxOut(BigInt(100), script);
+        const txOutBn = new TxOutBn(txOut, 0n);
+        txOutBnMap.add(txOutBn, Buffer.alloc(32), i);
+      }
+
+      const changeScript = Script.fromEmpty();
+      txBuilder = new TxBuilder(txOutBnMap, changeScript, 0n);
+    });
+
+    test("should sign and verify expired pkhxr 90d 60d", () => {
+      const expiredInputScript = Script.fromExpiredPkhxrInput();
+      const txIn = new TxIn(
+        Buffer.alloc(32),
+        0,
+        expiredInputScript,
+        Script.PKHXR_90D_60D_X_LOCK_REL,
+      );
+      txBuilder.addInput(txIn, 100n);
+
+      const txOut = new TxOut(BigInt(50), Script.fromEmpty());
+      txBuilder.addOutput(txOut);
+
+      const tx = txBuilder.build().unwrap();
+
+      expect(tx.inputs.length).toBe(1);
+      expect(tx.outputs.length).toBe(2);
+      expect(tx.outputs[0].value).toBe(BigInt(50));
+      expect(tx.outputs[1].value).toBe(BigInt(50));
+
+      txSigner = new TxSigner(
+        tx,
+        txOutBnMap,
+        pkhKeyMap,
+        BigInt(Script.PKHXR_90D_60D_X_LOCK_REL),
+      );
+      const signed = txSigner.sign(0);
+      expect(signed.ok).toBe(true);
+      expect(tx.inputs[0].script.isExpiredPkhxrInput()).toBe(true);
+
+      const txVerifier = new TxVerifier(
+        tx,
+        txOutBnMap,
+        BigInt(Script.PKHXR_90D_60D_X_LOCK_REL),
+      );
+      const verifiedInputScript = txVerifier.verifyInputScript(0);
+      expect(verifiedInputScript).toBe(true);
+
+      const verifiedInputLockRel = txVerifier.verifyInputLockRel(0);
+      expect(verifiedInputLockRel).toBe(true);
+
+      const verifiedScripts = txVerifier.verifyInputs();
+      expect(verifiedScripts).toBe(true);
+
+      const verifiedOutputValues = txVerifier.verifyOutputValues();
+      expect(verifiedOutputValues).toBe(true);
+
+      const verified = txVerifier.verify();
+      expect(verified).toBe(true);
+    });
+  });
 });
