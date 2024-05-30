ba0ff7ef2d61d9ab442eebf68e8780dce20851c4
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-30 15:44:52 -0500

    remove unused iso buf packages
    after experimenting with a new package for IsoBuf, i've decided the simplest
    solution is to leave the rust code a lone, and make a very small change to the
    typescript code, where we rename the npm package "Buffer" to "IsoBuf". this
    will make it a lot easier to spot any unintentional uses of the node global
    Buffer, which we should never use. meanwhile, the only other use case for the
    iso buf packages was for strict hex, and i've decided to just keep doing it the
    same way i am already doing it with a very simple strict hex module.

--- rs/earthbucks_iso_buf/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod iso_buf_error;
-pub mod reader;
-pub mod iso_buf;
-pub mod writer;
--- ts/earthbucks-iso-buf/src/lib.ts
+++
@@ -1,4 +0,0 @@
-export * from "./iso-buf-error.js";
-export * from "./iso-buf.js";
-export * from "./reader.js";
-export * from "./writer.js";
