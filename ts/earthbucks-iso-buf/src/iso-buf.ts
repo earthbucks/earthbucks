fca528caa1293f091c2331d43b04c6b3a8329653
Ryan X. Charles <ryan@ryanxcharles.com>
2024-05-30 07:25:35 -0500

    refine names and types

--- ts/earthbucks-iso-buf/src/iso-buf.ts
+++ ts/earthbucks-iso-buf/src/iso-buf.ts
@@ -1,36 +1,65 @@
-export class IsoBuf {
-  public buf: Uint8Array
+import { Result, Ok, Err } from "earthbucks-opt-res";
 
-  constructor(buf: Uint8Array) {
-    this.buf = buf
-  }
-
-  static fromUint8Array(buf: Uint8Array): IsoBuf {
-    return new IsoBuf(buf)
-  }
-
-  toUint8Array(): Uint8Array {
-    return this.buf
-  }
-}
-
-export class IsoBufN<N extends number> {
+export class U8Arr {
   public buf: Uint8Array;
-  public size: N;
+  public size: number;
 
-  constructor(size: N, buf: Uint8Array) {
-    if (buf.length !== size) {
+  constructor(buf: Uint8Array, size?: number) {
+    if (size && buf.length !== size) {
       throw new Error(`Expected buffer of length ${size}, got ${buf.length}`);
     }
-    this.size = size;
+    this.size = size || buf.length;
     this.buf = buf;
-  }
-
-  static fromUint8Array<N extends number>(size: N, buf: Uint8Array): IsoBufN<N> {
-    return new IsoBufN(size, buf);
   }
 
   toUint8Array(): Uint8Array {
     return this.buf;
+  }
+}
+
+export class FixedU8<N extends number> extends U8Arr {
+  public size: N;
+
+  constructor(buf: Uint8Array, size: N) {
+    super(buf, size);
+    if (buf.length !== size) {
+      // returning Result is not possible inside a constructor
+      throw new Error(`Expected buffer of length ${size}, got ${buf.length}`);
+    }
+    this.size = size;
+  }
+
+  static fromUint8Array<N extends number>(
+    buf: Uint8Array,
+    size: N,
+  ): Result<FixedU8<N>, string> {
+    if (buf.length !== size) {
+      return Err(`Expected buffer of length ${size}, got ${buf.length}`);
+    }
+    return Ok(new FixedU8(buf, size));
+  }
+
+  toUint8Array(): Uint8Array {
+    return this.buf;
+  }
+}
+
+export class U8_32 extends U8Arr {
+  public size: 32 = 32;
+
+  constructor(buf: Uint8Array) {
+    const size = buf.length;
+    if (size !== 32) {
+      // returning Result is not possible inside a constructor
+      throw new Error(`Expected buffer of length 32, got ${size}`);
+    }
+    super(buf, size);
+  }
+
+  static fromUint8Array(buf: Uint8Array): Result<U8_32, string> {
+    if (buf.length !== 32) {
+      return Err(`Expected buffer of length 32, got ${buf.length}`);
+    }
+    return Ok(new U8_32(buf));
   }
 }
---
+++ ts/earthbucks-iso-buf/test/iso-buf.test.ts
@@ -0,0 +1,18 @@
+import { describe, test, expect } from "vitest";
+import { U8Arr, U8_32, FixedU8 } from "../src/iso-buf";
+
+describe("IsoBuf", () => {
+  test("it should make an iso buf of size 32", () => {
+    const isoBuf: FixedU8<32> = new FixedU8<32>(new Uint8Array(32), 32);
+    const isoBuf2: U8_32 = new U8_32(new Uint8Array(32));
+    const isoBuf3: U8_32 = U8_32.fromUint8Array(new Uint8Array(32)).unwrap();
+    const isobuf4: FixedU8<32> = FixedU8.fromUint8Array(
+      new Uint8Array(32),
+      32,
+    ).unwrap();
+    expect(isoBuf).toBeDefined();
+    expect(isoBuf2).toBeDefined();
+    expect(isoBuf3).toBeDefined();
+    expect(isobuf4).toBeDefined();
+  });
+});
