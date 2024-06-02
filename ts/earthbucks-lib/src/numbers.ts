9a8ea671fc4d5515194f124552ca6635e598ad60
Ryan X. Charles <ryan@ryanxcharles.com>
2024-06-02 05:48:09 -0500

    use abstract clas for basic numbers

--- ts/earthbucks-lib/src/numbers.ts
+++ ts/earthbucks-lib/src/numbers.ts
@@ -1,287 +1,368 @@
-export class U8 {
-  private value: number;
+abstract class BasicNumber<T, U extends BasicNumber<T, U>> {
+  protected value: T;
 
-  constructor(value: number) {
-    if (value < 0 || value > 255) {
-      throw new Error(`Value ${value} is not a valid u8`);
+  constructor(
+    value: T,
+    protected min: T,
+    protected max: T,
+  ) {
+    if (this.lessThan(value, min) || this.greaterThan(value, max)) {
+      throw new Error(`Value ${value} is not a valid number`);
     }
     this.value = value;
+  }
+
+  abstract add(other: U): U;
+  abstract mul(other: U): U;
+  abstract getValue(): T;
+  protected abstract lessThan(a: T, b: T): boolean;
+  protected abstract greaterThan(a: T, b: T): boolean;
+}
+
+export class U8 extends BasicNumber<number, U8> {
+  constructor(value: number) {
+    super(value, 0, 255);
   }
 
   add(other: U8): U8 {
-    return new U8(this.value + other.value);
+    const result = this.value + other.value;
+    return new U8(result);
   }
 
   mul(other: U8): U8 {
-    return new U8(this.value * other.value);
+    const result = this.value * other.value;
+    return new U8(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class I8 {
-  private value: number;
-
+export class I8 extends BasicNumber<number, I8> {
   constructor(value: number) {
-    if (value < -128 || value > 127) {
-      throw new Error(`Value ${value} is not a valid i8`);
-    }
-    this.value = value;
+    super(value, -128, 127);
   }
 
   add(other: I8): I8 {
-    return new I8(this.value + other.value);
+    const result = this.value + other.value;
+    return new I8(result);
   }
 
   mul(other: I8): I8 {
-    return new I8(this.value * other.value);
+    const result = this.value * other.value;
+    return new I8(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class U16 {
-  private value: number;
-
+export class U16 extends BasicNumber<number, U16> {
   constructor(value: number) {
-    if (value < 0 || value > 65535) {
-      throw new Error(`Value ${value} is not a valid u16`);
-    }
-    this.value = value;
+    super(value, 0, 65535);
   }
 
   add(other: U16): U16 {
-    return new U16(this.value + other.value);
+    const result = this.value + other.value;
+    return new U16(result);
   }
 
   mul(other: U16): U16 {
-    return new U16(this.value * other.value);
+    const result = this.value * other.value;
+    return new U16(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class I16 {
-  private value: number;
-
+export class I16 extends BasicNumber<number, I16> {
   constructor(value: number) {
-    if (value < -32768 || value > 32767) {
-      throw new Error(`Value ${value} is not a valid i16`);
-    }
-    this.value = value;
+    super(value, -32768, 32767);
   }
 
   add(other: I16): I16 {
-    return new I16(this.value + other.value);
+    const result = this.value + other.value;
+    return new I16(result);
   }
 
   mul(other: I16): I16 {
-    return new I16(this.value * other.value);
+    const result = this.value * other.value;
+    return new I16(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class U32 {
-  private value: number;
-
+export class U32 extends BasicNumber<number, U32> {
   constructor(value: number) {
-    if (value < 0 || value > 4294967295) {
-      throw new Error(`Value ${value} is not a valid u32`);
-    }
-    this.value = value;
+    super(value, 0, 4294967295);
   }
 
   add(other: U32): U32 {
-    return new U32(this.value + other.value);
+    const result = this.value + other.value;
+    return new U32(result);
   }
 
   mul(other: U32): U32 {
-    return new U32(this.value * other.value);
+    const result = this.value * other.value;
+    return new U32(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class I32 {
-  private value: number;
-
+export class I32 extends BasicNumber<number, I32> {
   constructor(value: number) {
-    if (value < -2147483648 || value > 2147483647) {
-      throw new Error(`Value ${value} is not a valid i32`);
-    }
-    this.value = value;
+    super(value, -2147483648, 2147483647);
   }
 
   add(other: I32): I32 {
-    return new I32(this.value + other.value);
+    const result = this.value + other.value;
+    return new I32(result);
   }
 
   mul(other: I32): I32 {
-    return new I32(this.value * other.value);
+    const result = this.value * other.value;
+    return new I32(result);
   }
 
   getValue(): number {
     return this.value;
   }
+
+  protected lessThan(a: number, b: number): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: number, b: number): boolean {
+    return a > b;
+  }
 }
 
-export class U64 {
-  private value: bigint;
-
+export class U64 extends BasicNumber<bigint, U64> {
   constructor(value: bigint) {
-    if (value < 0n || value > 18446744073709551615n) {
-      throw new Error(`Value ${value} is not a valid u64`);
-    }
-    this.value = value;
+    super(value, 0n, 18446744073709551615n);
   }
 
   add(other: U64): U64 {
-    return new U64(this.value + other.value);
+    const result = this.value + other.value;
+    return new U64(result);
   }
 
   mul(other: U64): U64 {
-    return new U64(this.value * other.value);
+    const result = this.value * other.value;
+    return new U64(result);
   }
 
   getValue(): bigint {
     return this.value;
   }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
+  }
 }
 
-export class I64 {
-  private value: bigint;
-
+export class I64 extends BasicNumber<bigint, I64> {
   constructor(value: bigint) {
-    if (value < -9223372036854775808n || value > 9223372036854775807n) {
-      throw new Error(`Value ${value} is not a valid i64`);
-    }
-    this.value = value;
+    super(value, -9223372036854775808n, 9223372036854775807n);
   }
 
   add(other: I64): I64 {
-    return new I64(this.value + other.value);
+    const result = this.value + other.value;
+    return new I64(result);
   }
 
   mul(other: I64): I64 {
-    return new I64(this.value * other.value);
+    const result = this.value * other.value;
+    return new I64(result);
   }
 
   getValue(): bigint {
     return this.value;
   }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
+  }
 }
 
-export class U128 {
-  private value: bigint;
-
+export class U128 extends BasicNumber<bigint, U128> {
   constructor(value: bigint) {
-    if (value < 0n || value > 340282366920938463463374607431768211455n) {
-      throw new Error(`Value ${value} is not a valid u128`);
-    }
-    this.value = value;
+    super(value, 0n, 340282366920938463463374607431768211455n);
   }
 
   add(other: U128): U128 {
-    return new U128(this.value + other.value);
+    const result = this.value + other.value;
+    return new U128(result);
   }
 
   mul(other: U128): U128 {
-    return new U128(this.value * other.value);
+    const result = this.value * other.value;
+    return new U128(result);
   }
 
   getValue(): bigint {
     return this.value;
   }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
+  }
 }
 
-export class I128 {
-  private value: bigint;
-
+export class I128 extends BasicNumber<bigint, I128> {
   constructor(value: bigint) {
-    if (
-      value < -170141183460469231731687303715884105728n ||
-      value > 170141183460469231731687303715884105727n
-    ) {
-      throw new Error(`Value ${value} is not a valid i128`);
-    }
-    this.value = value;
+    super(
+      value,
+      -170141183460469231731687303715884105728n,
+      170141183460469231731687303715884105727n,
+    );
   }
 
   add(other: I128): I128 {
-    return new I128(this.value + other.value);
+    const result = this.value + other.value;
+    return new I128(result);
   }
 
   mul(other: I128): I128 {
-    return new I128(this.value * other.value);
+    const result = this.value * other.value;
+    return new I128(result);
   }
 
   getValue(): bigint {
     return this.value;
   }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
+  }
 }
 
-export class U256 {
-  private value: bigint;
-
+export class U256 extends BasicNumber<bigint, U256> {
   constructor(value: bigint) {
-    if (
-      value < 0n ||
-      value >
-        115792089237316195423570985008687907853269984665640564039457584007913129639935n
-    ) {
-      throw new Error(`Value ${value} is not a valid u256`);
-    }
-    this.value = value;
+    super(
+      value,
+      0n,
+      115792089237316195423570985008687907853269984665640564039457584007913129639935n,
+    );
   }
 
   add(other: U256): U256 {
-    return new U256(this.value + other.value);
+    const result = this.value + other.value;
+    return new U256(result);
   }
 
   mul(other: U256): U256 {
-    return new U256(this.value * other.value);
+    const result = this.value * other.value;
+    return new U256(result);
   }
 
   getValue(): bigint {
     return this.value;
+  }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
   }
 }
 
-export class I256 {
-  private value: bigint;
-
+export class I256 extends BasicNumber<bigint, I256> {
   constructor(value: bigint) {
-    if (
-      value <
-        -57896044618658097711785492504343953926634992332820282019728792003956564819968n ||
-      value >
-        57896044618658097711785492504343953926634992332820282019728792003956564819967n
-    ) {
-      throw new Error(`Value ${value} is not a valid i256`);
-    }
-    this.value = value;
+    super(
+      value,
+      -57896044618658097711785492504343953926634992332820282019728792003956564819968n,
+      57896044618658097711785492504343953926634992332820282019728792003956564819967n,
+    );
   }
 
   add(other: I256): I256 {
-    return new I256(this.value + other.value);
+    const result = this.value + other.value;
+    return new I256(result);
   }
 
   mul(other: I256): I256 {
-    return new I256(this.value * other.value);
+    const result = this.value * other.value;
+    return new I256(result);
   }
 
   getValue(): bigint {
     return this.value;
+  }
+
+  protected lessThan(a: bigint, b: bigint): boolean {
+    return a < b;
+  }
+
+  protected greaterThan(a: bigint, b: bigint): boolean {
+    return a > b;
   }
 }
