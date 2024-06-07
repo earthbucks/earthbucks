8e149c0aafba2ad5c15015c7724c43ba1ce1d45f
Ryan X. Charles <ryan@ryanxcharles.com>
2024-06-07 16:36:54 -0500

    yay, first working example of adding two numbers!
    rust tensorflow works

--- rs/earthbucks_pow/src/main.rs
+++ rs/earthbucks_pow/src/main.rs
@@ -1,5 +1,5 @@
 use tensorflow::ops;
-use tensorflow::Graph;
+// use tensorflow::Graph;
 use tensorflow::Scope;
 use tensorflow::Session;
 use tensorflow::SessionOptions;
@@ -7,18 +7,35 @@ use tensorflow::SessionRunArgs;
 use tensorflow::Tensor;
 
 fn main() {
-    let graph = Graph::new();
+    // our goal is to write a test program using rust tensorflow
+    // it is very simple: we want to add two numbers together
+
+    // first, we need to create a scope
+    let scope = &mut Scope::new_root_scope();
+
+    // now we need to create two constants
+    let a = ops::constant(3.0f32, scope).unwrap();
+    let b = ops::constant(4.0f32, scope).unwrap();
+
+    // now we need to add the two constants together
+    let c = ops::add(a, b, scope).unwrap();
+
+    // now we need to create a session
+    let graph = scope.graph();
     let session = Session::new(&SessionOptions::new(), &graph).unwrap();
 
-    let mut root = Scope::new_root_scope();
-    let a = ops::constant(Tensor::new(&[1]).with_values(&[1]).unwrap(), &mut root).unwrap();
-    let b = ops::constant(Tensor::new(&[1]).with_values(&[2]).unwrap(), &mut root).unwrap();
-    let c = ops::add(a, b, &mut root).unwrap();
-
+    // now we need to run the session
     let mut args = SessionRunArgs::new();
+
     // Request a fetch for the output of the add operation
     let token = args.request_fetch(&c, 0);
+
+    // now we need to run the session
     session.run(&mut args).unwrap();
-    let result_tensor: Tensor<i32> = args.fetch(token).unwrap();
-    println!("{:?}", result_tensor.to_vec()[0]);
+
+    // now we need to get the result
+    let result: Tensor<f32> = args.fetch(token).unwrap();
+
+    // now we need to print the result
+    println!("result: {}", result[0]);
 }
