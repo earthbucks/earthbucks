13febe30cdf2f3b5a8023a722f31c5e474f53e40
Ryan X. Charles <ryan@ryanxcharles.com>
2024-06-08 02:42:22 -0500

    testing add & mul

--- rs/earthbucks_pow/src/main.rs
+++ rs/earthbucks_pow/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use tensorflow::ops;
 // use tensorflow::Graph;
 use tensorflow::Scope;
@@ -40,6 +41,52 @@ fn add() {
     println!("result: {}", result[0]);
 }
 
+fn mul() {
+    // our goal is to write a test program using rust tensorflow
+    // it is very simple: we want to add two numbers together
+
+    // first, we need to create a scope
+    let scope = &mut Scope::new_root_scope();
+
+    // now we need to create two constants
+    let a = ops::constant(3.0f32, scope).unwrap();
+    let b = ops::constant(4.0f32, scope).unwrap();
+
+    // now we need to add the two constants together
+    let c = ops::mul(a, b, scope).unwrap();
+
+    // now we need to create a session
+    let graph = scope.graph();
+    let session = Session::new(&SessionOptions::new(), &graph).unwrap();
+
+    // now we need to run the session
+    let mut args = SessionRunArgs::new();
+
+    // Request a fetch for the output of the add operation
+    let token = args.request_fetch(&c, 0);
+
+    // now we need to run the session
+    session.run(&mut args).unwrap();
+
+    // now we need to get the result
+    let result: Tensor<f32> = args.fetch(token).unwrap();
+
+    // now we need to print the result
+    println!("result: {}", result[0]);
+}
+
 fn main() {
-    add();
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if an argument was provided
+    if args.len() > 1 {
+        match args[1].as_str() {
+            "add" => add(),
+            "mul" => mul(), // assuming you have a function named mul
+            _ => println!("Unknown command"),
+        }
+    } else {
+        println!("Please provide a command");
+    }
 }
